2974abd5ae7de6eb563b457eec6acf548cfbfa38
Alex Phelps <alexphelps3@gmail.com>
2021-06-03 15:24:24 +0700

    refactor to use transaction receipts instead of debug api and add more contract addresses

--- exploring/latest_block_contract_gas.py
+++ exploring/latest_block_contract_gas.py
@@ -1,69 +1,70 @@
 from timeit import default_timer as timer
 import pandas as pd
 from web3 import Web3
-
+# WEB3_PROVIDER_ADDRESS = 'https://mainnet.infura.io/v3/ff78861e0d034bb78485a76fc24dcd20'
 WEB3_PROVIDER_ADDRESS = 'http://192.168.1.104:8545'
 CONTRACT_ADDRESSES_CSV = 'exploring/contract_address.csv'
 
-
-def analyze_block_gas_usage(block_number):
+def analyze_block_transaction_recipts(block_number):
     w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_ADDRESS))
     block = w3.eth.get_block(block_number, True)
+    block_trx_receitps = [w3.eth.get_transaction_receipt(each.hash) for each in block.transactions]
+    block_trx_data = [
+        {'hash': each.get('hash'),'to': str(each.get('to')).lower(),
+        'gasUsed':each.get('gasUsed')} for each in block_trx_receitps]
+    return block_trx_data
 
-    block_trace = w3.provider.make_request(
-        'debug_traceBlockByNumber', [Web3.toHex(block.number), {'tracer': "callTracer"}]
-    )
-    # drop the first level of depth
-    block_trace_transactions = [each.get('result') for each in block_trace.get('result')]
+def analyze_latest_block_gas():
+    w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_ADDRESS))
+    block = w3.eth.get_block('latest', True)
+    if not w3.eth.is_syncing():
 
-    if block_trace_transactions:
-        # format block data gasUsed from Hex to int
-        gas_converted_transactions = [
-            {'to': str(t['to']).lower(), 'gasUsed': Web3.toInt(hexstr=t['gasUsed'])} for t in block_trace_transactions]
-
-        # aggregate, sum, and count all transaction data by to address
-        block_transactions_df = pd.DataFrame(gas_converted_transactions)
-        block_transactions = block_transactions_df.groupby(['to', ]).agg(
-            gasUsed=('gasUsed', 'sum'), blocktransactions=('to', 'count'))
+        start_time = timer()
+        block_trx_data = analyze_block_transaction_recipts(block.number)
+        end_time = timer()
+        block_trx_df = pd.DataFrame(block_trx_data)
+        block_trx_summary = block_trx_df.groupby(['to', ]).agg(
+                gasUsed=('gasUsed', 'sum'), trxs=('to', 'count'))
+        simple_trx = block_trx_df['gasUsed'] == 21000
+        simple_trx_gas_used = simple_trx.sum() * 21000
 
         # format contract addresses to catch inconsistencies before merging data
         contract_addresses = pd.read_csv(CONTRACT_ADDRESSES_CSV)
         contract_addresses['address'] = contract_addresses['address'].apply(lambda x: str(x).lower())
+        # drop any duplicates, keep the first
+        contract_addresses = contract_addresses.drop_duplicates(subset='address', keep='first')
 
         #  merge transaction receipts with dapp contract addresses
-        result = block_transactions.merge(contract_addresses, left_on='to', right_on='address')
+        result = block_trx_summary.merge(contract_addresses, left_on='to', right_on='address')
 
-    else:
-        result = 'No Transactions'
-    return result
-
-def analyze_latest_block_gas():
-    w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_ADDRESS))
-    block = w3.eth.get_block('latest')
-    if not w3.eth.is_syncing():
-        print('=== Block Info ===')
+        print('\n=== Block Info ===')
         print('Block Number - {}'.format(block.number))
-        print('Block Transaction Count - {}'.format(len(block.transactions)))
-        start_time = timer()
-        result = analyze_block_gas_usage(block.number)
-        dapp_grouped = result.groupby(['dapp',]).agg(
-            gasUsed=('gasUsed', 'sum'), blocktransactions=('blocktransactions', 'sum'))
-        end_time = timer()
+        print('Block Transactions - {}'.format(len(block.transactions)))
+        print('\n=== Simple Transaction Gas Used ===')
+        print(simple_trx_gas_used)
 
-        print('\n=== Known Contract Gas Used ===')
+        print('\n=== Dapp Contract Gas Used ===')
         print(result)
-        print('\n=== Summary ===')
+
+        print('\n=== Dapp Summary ===')
+        dapp_grouped = result.groupby(['dapp',]).agg(
+            gasUsed=('gasUsed', 'sum'), trxs=('trxs', 'sum'))
+        dapp_gas_used = dapp_grouped['gasUsed'].sum()
+        print('Dapp Gas Used - {}'.format(dapp_gas_used))
         print(dapp_grouped)
-        known_gasUsed = dapp_grouped['gasUsed'].sum()
 
         print('\n=== Debug Info ===')
-        print('Analyzed Block Data in {:.3f}s'.format(end_time - start_time))
+        print('Block Number - {}'.format(block.number))
+        print('Block Transactions - {}'.format(len(block.transactions)))
+        print('Pulled Block Data in {:.3f}s'.format(end_time - start_time))
         print('Total Block Gas Used - {}'.format(block.get('gasUsed')))
-        print('Known Block Gas Used - {}'.format(known_gasUsed))
-        known_gasUsed_percent = 'Known Gas Used Percent - {:.0%}'.format((known_gasUsed / block.get('gasUsed')))
-        print(known_gasUsed_percent)
+        print('Simple Transaction Gas Used - {}'.format(simple_trx_gas_used))
+        print('Dapp Block Gas Used - {}'.format(dapp_grouped['gasUsed'].sum()))
+        print('Simple Transaction Gas Used Percent - {:.0%}'.format((simple_trx_gas_used / block.get('gasUsed'))))
+        print('Dapp Gas Used Percent - {:.0%}'.format((dapp_gas_used / block.get('gasUsed'))))
+        print('Attributed Gas Used Percent - {:.0%}'.format(((dapp_gas_used + simple_trx_gas_used) / block.get('gasUsed'))))
+
     else:
         print(w3.eth.is_syncing())
-
 
 analyze_latest_block_gas()
