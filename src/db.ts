df57f55bdc8bab35c10f85c02dfb4a413bb7c7c9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-26 18:09:15 +0200

    Clean up unused env var

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,6 @@ const database = Config.network === "ropsten" ? "ropsten" : "defaultdb";
 
 export const sql = postgres({
   host: process.env.DB_HOST,
-  // database: process.env.DB_DATABASE,
   database,
   password: process.env.DB_PASSWORD,
   username: process.env.DB_USER,
