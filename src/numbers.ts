1e24d114606f6082287012da329a3874d5fbdbd0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-07-28 17:16:07 +0200

    fix number to hex by prefixing 0x

--- src/numbers.ts
+++ src/numbers.ts
@@ -1,6 +1,6 @@
 export const hexToNumber = (hex: string) => Number.parseInt(hex, 16);
 
-export const numberToHex = (num: number) => num.toString(16);
+export const numberToHex = (num: number) => `0x${num.toString(16)}`;
 
 export const weiToEth = (wei: number): number => wei / 10 ** 18;
 
