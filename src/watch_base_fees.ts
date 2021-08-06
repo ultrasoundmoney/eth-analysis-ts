90d3e17fe1990019963d387fcc52873c289f4865
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-06 13:59:58 +0200

    use named module import

--- src/watch_base_fees.ts
+++ src/watch_base_fees.ts
@@ -1,12 +1,12 @@
 import * as BaseFees from "./base_fees.js";
 import * as Log from "./log.js";
 import { sql } from "./db.js";
-import { closeWeb3Ws } from "./web3.js";
+import * as eth from "./web3.js";
 
 BaseFees.watchAndCalcBaseFees()
   .then(async () => {
     Log.info("> done analyzing gas");
-    closeWeb3Ws();
+    eth.closeWeb3Ws();
     await sql.end();
   })
   .catch((error) => {
