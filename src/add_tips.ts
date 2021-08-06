0b991b4142076235f25ddae81f6087af719717be
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-06 14:17:01 +0200

    add progress bar to tip adding

--- src/add_tips.ts
+++ src/add_tips.ts
@@ -3,6 +3,7 @@ import { sql } from "./db.js";
 import * as BaseFees from "./base_fees.js";
 import * as Transactions from "./transactions.js";
 import * as eth from "./web3.js";
+import ProgressBar from "progress";
 
 const addTipsToAnalyzedBlocks = async (): Promise<void> => {
   const blocksMissingTips = await sql<{ number: number }[]>`
@@ -11,6 +12,10 @@ const addTipsToAnalyzedBlocks = async (): Promise<void> => {
   `.then((rows) => rows.map((row) => row.number));
 
   Log.info(`adding tips for ${blocksMissingTips.length} blocks`);
+
+  const bar = new ProgressBar("[:bar] :rate/s :percent :etas", {
+    total: blocksMissingTips.length,
+  });
 
   for (const blockNumber of blocksMissingTips) {
     const block = await eth.getBlock(blockNumber);
@@ -21,6 +26,7 @@ const addTipsToAnalyzedBlocks = async (): Promise<void> => {
       SET tips = ${tips}
       WHERE number = ${block.number}
     `;
+    bar.tick();
   }
 };
 
