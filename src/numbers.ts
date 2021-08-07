19d0960ff09666539ae13f041dca874d3fea95a9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-07 11:58:38 +0200

    add big int variants

--- src/numbers.ts
+++ src/numbers.ts
@@ -6,5 +6,9 @@ export const weiToEth = (wei: number): number => wei / 10 ** 18;
 
 export const weiToGwei = (wei: number): number => wei / 10 ** 9;
 
+export const weiToEthBI = (wei: bigint): bigint => wei / BigInt(10 ** 18);
+
+export const weiToGweiBI = (wei: bigint): bigint => wei / BigInt(10 ** 9);
+
 export const sum = (nums: readonly number[]) =>
   nums.reduce((sum, num) => sum + num, 0);
