7ff1beff74cd24ef98bb4da8f599d51afc4fcad8
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-17 18:10:27 +0200

    remove dapp logic from base fee totals

--- src/base_fee_totals.ts
+++ src/base_fee_totals.ts
@@ -1,7 +1,3 @@
-import neatCsv from "neat-csv";
-import type { Options as NeatCsvOptions } from "neat-csv";
-import fs from "fs/promises";
-import T from "fp-ts/lib/Task.js";
 import A from "fp-ts/lib/Array.js";
 import { flow, pipe } from "fp-ts/lib/function.js";
 import { sql } from "./db.js";
@@ -13,8 +9,6 @@ import type {
 import { differenceInSeconds } from "date-fns";
 import * as Log from "./log.js";
 import * as BaseFees from "./base_fees.js";
-import NEA from "fp-ts/lib/NonEmptyArray.js";
-import R from "fp-ts/lib/Record.js";
 import { sum } from "./numbers.js";
 import * as eth from "./web3.js";
 import type { BlockLondon } from "./web3.js";
@@ -22,65 +16,7 @@ import { delay } from "./delay.js";
 import * as Transactions from "./transactions.js";
 import Sentry from "@sentry/node";
 
-type DappId = string;
 type ContractAddress = string;
-type DappAddress = { dapp_id: DappId; address: ContractAddress };
-type AddressToDappMap = Partial<Record<string, string>>;
-let cAddressToDappMap: AddressToDappMap | undefined = undefined;
-
-const readFile =
-  (path: string): T.Task<Buffer> =>
-  () =>
-    fs.readFile(path);
-
-const readCsv =
-  <A>(csv: Buffer, options?: NeatCsvOptions): T.Task<A[]> =>
-  () =>
-    neatCsv<A>(csv, options);
-
-const getAddressToDappMap = async () => {
-  if (cAddressToDappMap !== undefined) {
-    return cAddressToDappMap;
-  }
-
-  return pipe(
-    readFile("./dapp_addresses.csv"),
-    T.chain((csv) => readCsv<DappAddress>(csv)),
-    T.map(
-      A.reduce({} as AddressToDappMap, (map, { dapp_id, address }) => {
-        map[address] = dapp_id;
-        return map;
-      }),
-    ),
-    T.map((addressToDappMap) => {
-      cAddressToDappMap = addressToDappMap;
-      return addressToDappMap;
-    }),
-  )();
-};
-
-type DappToAddressesMap = Record<string, string[]>;
-let cDappToAddressesMap: DappToAddressesMap | undefined = undefined;
-const getDappToAddressesMap = async () => {
-  if (cDappToAddressesMap !== undefined) {
-    return cDappToAddressesMap;
-  }
-
-  const dappToAddressesMap = await pipe(
-    readFile("./dapp_addresses.csv"),
-    T.chain((csv) => readCsv<DappAddress>(csv)),
-    T.map(NEA.groupBy((dappAddress) => dappAddress.dapp_id)),
-    T.map(R.map(A.map((dappAddress) => dappAddress.address))),
-    T.map((dappToAdressesMap) => {
-      cDappToAddressesMap = dappToAdressesMap;
-      return dappToAdressesMap;
-    }),
-  )();
-
-  cDappToAddressesMap = dappToAddressesMap;
-
-  return dappToAddressesMap;
-};
 
 type AnalyzedBlock = {
   number: number;
@@ -132,42 +68,14 @@ const getTimeframeSegments = (blocks: AnalyzedBlock[]): Segments => {
   };
 };
 
-const groupByDapp = (
-  dappAddressMap: AddressToDappMap,
-  sumsByContract: Record<string, number>,
-): {
-  dappSums: Map<DappId, number>;
-  contractSums: Map<ContractAddress, number>;
-} => {
-  const dappSums = new Map();
-  const contractSums = new Map();
-
-  Object.entries(sumsByContract).forEach(([address, feesForAddress]) => {
-    const dapp = dappAddressMap[address];
-    if (dapp) {
-      const sum = dappSums.get(address) || 0;
-      dappSums.set(dapp, sum + feesForAddress);
-      return;
-    }
-
-    const sum = contractSums.get(address) || 0;
-    contractSums.set(address, sum + feesForAddress);
-    return;
-  });
-
-  return { dappSums, contractSums };
-};
-
 const writeSums = async (
   timeframe: Timeframe,
-  sums: Map<string, number>,
+  sums: Record<ContractAddress, number>,
   oldestIncludedBlock: number,
-  totalType: TotalType,
 ) => {
-  const table = getTableName(totalType, timeframe);
-  const idColumn = totalIdColumnMap[totalType];
-  const sumsInserts = Array.from(sums).map(([id, feeTotal]) => ({
-    [idColumn]: id,
+  const table = getTableName(timeframe);
+  const sumsInserts = Object.entries(sums).map(([id, feeTotal]) => ({
+    contract_address: id,
     fee_total: feeTotal,
     oldest_included_block: oldestIncludedBlock,
   }));
@@ -186,13 +94,11 @@ const writeSums = async (
   }
 
   Log.debug(
-    `done writing sums for ${totalType} - ${timeframe}, ${sumsInserts.length} written`,
+    `done writing sums for contract - ${timeframe}, ${sumsInserts.length} written`,
   );
 };
 
 export const calcTotals = async (upToIncludingBlockNumber: number) => {
-  const dappAddressMap = await getAddressToDappMap();
-
   Log.debug(
     `fetching all base fees per block up to and including: ${upToIncludingBlockNumber}`,
   );
@@ -243,106 +149,55 @@ export const calcTotals = async (upToIncludingBlockNumber: number) => {
     BaseFees.sumFeeMaps,
   );
 
-  const { dappSums: dappSums1h, contractSums: contractSums1h } = groupByDapp(
-    dappAddressMap,
-    sumByContract1h,
-  );
-
-  const { dappSums: dappSums24h, contractSums: contractSums24h } = groupByDapp(
-    dappAddressMap,
-    sumByContract24h,
-  );
-
-  const { dappSums: dappSums7d, contractSums: contractSums7d } = groupByDapp(
-    dappAddressMap,
-    sumByContract7d,
-  );
-
-  const { dappSums: dappSums30d, contractSums: contractSums30d } = groupByDapp(
-    dappAddressMap,
-    sumByContract30d,
-  );
-
-  const { dappSums: dappSumsAll, contractSums: contractSumsAll } = groupByDapp(
-    dappAddressMap,
-    sumByContractAll,
-  );
-
-  Log.debug(`found ${dappSumsAll.size} dapps with accumulated base fees`);
+  const contractAddressesAll = Object.keys(sumByContractAll);
   Log.debug(
-    `found ${contractSumsAll.size} unknown contracts with accumulated base fees`,
+    `found ${contractAddressesAll.length} contracts with accumulated base fees`,
   );
 
-  await ensureContractAddressKnown(Object.keys(sumByContractAll));
+  await ensureContractAddressKnown(contractAddressesAll);
 
   if (oldestBlock1h !== undefined) {
-    await sql`TRUNCATE dapp_1h_totals;`;
     await sql`TRUNCATE contract_1h_totals;`;
-    await writeSums("1h", dappSums1h, oldestBlock1h.number, "dapp");
-    await writeSums("1h", contractSums1h, oldestBlock1h.number, "contract");
+    await writeSums("1h", sumByContract1h, oldestBlock1h.number);
   } else {
     Log.warn("no oldest block within 1h found! are we 1h behind?");
   }
   if (oldestBlock24h !== undefined) {
-    await sql`TRUNCATE dapp_24h_totals;`;
     await sql`TRUNCATE contract_24h_totals;`;
-    await writeSums("24h", dappSums24h, oldestBlock24h.number, "dapp");
-    await writeSums("24h", contractSums24h, oldestBlock24h.number, "contract");
+    await writeSums("24h", sumByContract24h, oldestBlock24h.number);
   } else {
     Log.warn("no oldest block within 24h found! are we 24h behind?");
   }
-  await sql`TRUNCATE dapp_7d_totals;`;
   await sql`TRUNCATE contract_7d_totals;`;
-  await writeSums("7d", dappSums7d, oldestBlock7d.number, "dapp");
-  await writeSums("7d", contractSums7d, oldestBlock7d.number, "contract");
-  await sql`TRUNCATE dapp_30d_totals;`;
+  await writeSums("7d", sumByContract7d, oldestBlock7d.number);
   await sql`TRUNCATE contract_30d_totals;`;
-  await writeSums("30d", dappSums30d, oldestBlock30d.number, "dapp");
-  await writeSums("30d", contractSums30d, oldestBlock30d.number, "contract");
-  await sql`TRUNCATE dapp_all_totals;`;
+  await writeSums("30d", sumByContract30d, oldestBlock30d.number);
   await sql`TRUNCATE contract_all_totals;`;
-  await writeSums("all", dappSumsAll, oldestBlockAll.number, "dapp");
-  await writeSums("all", contractSumsAll, oldestBlockAll.number, "contract");
+  await writeSums("all", sumByContractAll, oldestBlockAll.number);
 
   Log.info("done inserting totals");
 };
 
-const getTableName = (totalType: TotalType, timeframe: Timeframe) =>
-  `${totalType}_${timeframe}_totals`;
+const getTableName = (timeframe: Timeframe) => `contract_${timeframe}_totals`;
 
 const writeTotal = async (
   block: BlockLondon,
   timeframe: Timeframe,
   id: string,
   useBaseFee: number,
-  totalType: TotalType,
 ): Promise<void> => {
-  const table = getTableName(totalType, timeframe);
-  const idColumn = totalIdColumnMap[totalType];
+  const table = getTableName(timeframe);
   await sql`
       INSERT INTO ${sql(table)} AS t (
-        ${sql(idColumn)},
+        contract_address,
         fee_total,
         oldest_included_block
       )
       VALUES (${id}, ${useBaseFee}, ${block.number})
-      ON CONFLICT (${sql(idColumn)}) DO UPDATE
+      ON CONFLICT (contract_address) DO UPDATE
         SET fee_total = t.fee_total + ${useBaseFee}`;
   return undefined;
 };
-
-const writeDappTotals = async (
-  block: BlockLondon,
-  dapp: string,
-  useBaseFee: number,
-): Promise<void> =>
-  Promise.all([
-    writeTotal(block, "1h", dapp, useBaseFee, "dapp"),
-    writeTotal(block, "24h", dapp, useBaseFee, "dapp"),
-    writeTotal(block, "7d", dapp, useBaseFee, "dapp"),
-    writeTotal(block, "30d", dapp, useBaseFee, "dapp"),
-    writeTotal(block, "all", dapp, useBaseFee, "dapp"),
-  ]).then(() => undefined);
 
 const writeContractTotals = async (
   block: BlockLondon,
@@ -350,60 +205,25 @@ const writeContractTotals = async (
   useBaseFee: number,
 ): Promise<void> =>
   Promise.all([
-    writeTotal(block, "1h", address, useBaseFee, "contract"),
-    writeTotal(block, "24h", address, useBaseFee, "contract"),
-    writeTotal(block, "7d", address, useBaseFee, "contract"),
-    writeTotal(block, "30d", address, useBaseFee, "contract"),
-    writeTotal(block, "all", address, useBaseFee, "contract"),
+    writeTotal(block, "1h", address, useBaseFee),
+    writeTotal(block, "24h", address, useBaseFee),
+    writeTotal(block, "7d", address, useBaseFee),
+    writeTotal(block, "30d", address, useBaseFee),
+    writeTotal(block, "all", address, useBaseFee),
   ]).then(() => undefined);
-
-const segmentBaseFeeTotalType = (
-  addressToDappMap: AddressToDappMap,
-  baseFees: FeeBreakdown,
-): { dappFees: [string, number][]; unknownDappFees: [string, number][] } => {
-  const useBaseFees = Object.entries(baseFees.contract_use_fees) as [
-    string,
-    number,
-  ][];
-
-  const dappFees = useBaseFees
-    .filter(([address]) => addressToDappMap[address] !== undefined)
-    .map(([address, useBaseFees]): [string, number] => [
-      addressToDappMap[address]!,
-      useBaseFees,
-    ]);
-  const unknownDappFees = useBaseFees.filter(
-    ([address]) => addressToDappMap[address] === undefined,
-  );
-
-  return { dappFees, unknownDappFees };
-};
 
 export const updateTotalsWithFees = async (
   block: BlockLondon,
   baseFees: FeeBreakdown,
 ) => {
-  const addressToDappMap = await getAddressToDappMap();
+  const unknownDappFees = baseFees;
 
-  const { dappFees, unknownDappFees } = segmentBaseFeeTotalType(
-    addressToDappMap,
-    baseFees,
-  );
-
-  await Promise.all([
-    ...dappFees.map(([dapp, fee]) => writeDappTotals(block, dapp, fee)),
-    ...unknownDappFees.map(([address, fee]) =>
-      writeContractTotals(block, address, fee),
+  await Promise.all(
+    Object.entries(unknownDappFees.contract_use_fees).map(([address, fee]) =>
+      writeContractTotals(block, address, fee!),
     ),
-    ensureFreshTotals("dapp", Object.keys(dappFees)),
-    ensureFreshTotals("contract", Object.keys(unknownDappFees)),
-  ]);
-};
-
-type TotalType = "dapp" | "contract";
-const totalIdColumnMap: Record<TotalType, string> = {
-  contract: "contract_address",
-  dapp: "dapp_id",
+  );
+  await ensureFreshTotals(Object.keys(unknownDappFees));
 };
 
 const timeframeHoursMap: Record<Timeframe, number> = {
@@ -415,13 +235,11 @@ const timeframeHoursMap: Record<Timeframe, number> = {
 };
 
 const subtractStaleBaseFees = async (
-  dappToAdressesMap: DappToAddressesMap,
   timeframe: Timeframe,
   oldestIncludedBlock: number,
-  totalType: TotalType,
   id: string,
 ) => {
-  const table = getTableName(totalType, timeframe);
+  const table = getTableName(timeframe);
   const maxHours = timeframeHoursMap[timeframe];
   const staleBlocks = await sql<{ number: number; baseFees: FeeBreakdown }[]>`
     SELECT number, base_fees FROM base_fees_per_block
@@ -431,12 +249,12 @@ const subtractStaleBaseFees = async (
   `;
 
   if (staleBlocks.length === 0) {
-    Log.debug(`no stale blocks for ${totalType} - ${id}`);
+    Log.debug(`no stale blocks for contract - ${id}`);
     return;
   }
 
   const { number: oldestFreshBlockNumber } = staleBlocks[0];
-  const addresses = totalType === "contract" ? [id] : dappToAdressesMap[id];
+  const addresses = [id];
   const staleSum = pipe(
     staleBlocks,
     A.map(
@@ -456,41 +274,14 @@ const subtractStaleBaseFees = async (
     SET
       fee_total = fee_total - ${staleSum},
       oldest_included_block = ${oldestFreshBlockNumber}
-    WHERE ${sql(totalIdColumnMap[totalType])} = ${id}`;
+    WHERE contract_address = ${id}`;
 };
 
 const ensureFreshTotal = async (
-  dappToAdressesMap: DappToAddressesMap,
   timeframe: Timeframe,
-  totalType: TotalType,
   ids: string[],
 ): Promise<void> => {
-  const table = getTableName(totalType, timeframe);
-  if (totalType === "dapp") {
-    const dappTotals = await sql<
-      {
-        dappId: string;
-        oldestIncludedBlock: number;
-      }[]
-    >`
-      SELECT oldest_included_block, dapp_id
-      FROM ${sql(table)}
-      JOIN base_fees_per_block ON oldest_included_block = number
-      WHERE dapp_id = ANY (${sql.array(ids)})`;
-
-    await Promise.all(
-      dappTotals.map((dappTotal) =>
-        subtractStaleBaseFees(
-          dappToAdressesMap,
-          timeframe,
-          dappTotal.oldestIncludedBlock,
-          "dapp",
-          dappTotal.dappId,
-        ),
-      ),
-    );
-    return;
-  }
+  const table = getTableName(timeframe);
 
   const contractTotals = await sql<
     {
@@ -506,105 +297,29 @@ const ensureFreshTotal = async (
   await Promise.all(
     contractTotals.map((contractTotal) =>
       subtractStaleBaseFees(
-        dappToAdressesMap,
         timeframe,
         contractTotal.oldestIncludedBlock,
-        "contract",
         contractTotal.contractAddress,
       ),
     ),
   );
 };
 
-const ensureFreshTotals = async (
-  totalType: TotalType,
-  dappsOrAddresses: string[],
-) => {
-  const dappToAdressesMap = await getDappToAddressesMap();
-  Log.debug(`removing stale fees for ${dappsOrAddresses.length} ${totalType}s`);
+const ensureFreshTotals = async (addresses: string[]) => {
+  Log.debug(`removing stale fees for ${addresses.length} contracts`);
 
   await Promise.all([
-    ensureFreshTotal(dappToAdressesMap, "1h", totalType, dappsOrAddresses),
-    ensureFreshTotal(dappToAdressesMap, "24h", totalType, dappsOrAddresses),
-    ensureFreshTotal(dappToAdressesMap, "7d", totalType, dappsOrAddresses),
-    ensureFreshTotal(dappToAdressesMap, "30d", totalType, dappsOrAddresses),
+    ensureFreshTotal("1h", addresses),
+    ensureFreshTotal("24h", addresses),
+    ensureFreshTotal("7d", addresses),
+    ensureFreshTotal("30d", addresses),
   ]);
 };
-
-// type DappName = { dapp_id: string; name: string };
-// type DappNameMap = Partial<Record<string, string>>;
-// let cDappNameMap: DappNameMap | undefined = undefined;
-// const getDappNameMap = async (): Promise<DappNameMap> => {
-//   if (cDappNameMap !== undefined) {
-//     return cDappNameMap;
-//   }
-
-//   return pipe(
-//     readFile("./dapp_names.csv"),
-//     T.chain((csv) => readCsv<DappName>(csv)),
-//     T.map(A.map(({ dapp_id, name }) => [dapp_id, name])),
-//     T.map(Object.fromEntries),
-//     T.map((dappNameMap) => {
-//       cDappNameMap = dappNameMap;
-//       return dappNameMap;
-//     }),
-//   )();
-// };
 
 export const getTopFeeBurners = async (
   timeframe: Timeframe,
 ): Promise<BaseFeeBurner[]> => {
-  // const maxHours = timeframeHoursMap[timeframe];
-  // const baseFeesPerBlock = await sql<{ baseFees: BlockBaseFees }[]>`
-  //     SELECT base_fees
-  //     FROM base_fees_per_block
-  //     WHERE now() - mined_at >= interval '${sql(String(maxHours))} hours'
-  // `.then((rows) => {
-  //   if (rows.length === 0) {
-  //     Log.warn(
-  //       "tried to determine top fee burners but found no analyzed blocks",
-  //     );
-  //     return [];
-  //   }
-
-  //   return rows.map((row) => row.baseFees);
-  // });
-
-  // const ethTransferBaseFees = pipe(
-  //   baseFeesPerBlock,
-  //   A.map((baseFees) => baseFees.transfers),
-  //   sum,
-  // );
-  // const contractCreationBaseFees = pipe(
-  //   baseFeesPerBlock,
-  //   A.map((baseFees) => baseFees.contract_creation_fees),
-  //   sum,
-  // );
-
-  // const tableDapps = getTableName("dapp", timeframe);
-
-  // const dappBurnerCandidatesRaw = await sql<
-  //   { dappId: string; feeTotal: BigInt }[]
-  // >`
-  //   SELECT dapp_id, fee_total FROM ${sql(tableDapps)}
-  //   ORDER BY fee_total DESC
-  //   LIMIT 11
-  // `.then((rows) =>
-  //   rows.map((row) => ({ ...row, feeTotal: Number(row.feeTotal) })),
-  // );
-
-  // const dappNameMap = await getDappNameMap();
-
-  // const dappBurnerCandidates: BaseFeeBurner[] = dappBurnerCandidatesRaw.map(
-  //   ({ dappId, feeTotal }) => ({
-  //     fees: feeTotal,
-  //     id: dappId,
-  //     name: dappNameMap[dappId] || dappId,
-  //     image: undefined,
-  //   }),
-  // );
-
-  const tableContracts = getTableName("contract", timeframe);
+  const tableContracts = getTableName(timeframe);
   const tableContractsSql = sql(tableContracts);
   const contractBurnerCandidatesRaw = await sql<
     {
@@ -648,7 +363,6 @@ export const getTopFeeBurners = async (
       //   image: undefined,
       //   name: "Contract deployments",
       // },
-      // ...dappBurnerCandidates,
       ...contractBurnerCandidates,
     ],
     A.sort<BaseFeeBurner>({
@@ -733,7 +447,7 @@ const ensureContractAddressKnown = async (addresses: string[]) => {
   }
 
   Log.debug(
-    `done ensuring contract addresses are known for ${addresses.length} addresses`,
+    `ensuring db contract entities exist for ${addresses.length} addresses with base fees`,
   );
 };
 
@@ -751,7 +465,7 @@ export const watchAndCalcTotalFees = async () => {
     throw new Error("no analyzed blocks, cannot calculate base fee totals");
   }
 
-  Log.debug("calculating base fee totals for all known dapps");
+  Log.debug("calculating base fee totals for all known contracts");
   await calcTotals(latestBlockNumberAtStart);
   Log.debug("done calculating fresh base fee totals");
   calcTotalsTransaction.finish();
@@ -764,6 +478,7 @@ export const watchAndCalcTotalFees = async () => {
   while (true) {
     const latestAnalyzedBlockNumber =
       await BaseFees.getLatestAnalyzedBlockNumber();
+    Log.debug(`latest analyzed block number: ${latestAnalyzedBlockNumber}`);
 
     if (latestAnalyzedBlockNumber === undefined) {
       throw new Error("no analyzed blocks, cannot calculate base fee totals");
@@ -788,21 +503,11 @@ export const watchAndCalcTotalFees = async () => {
     const txrs = await Transactions.getTxrsWithRetry(block);
 
     const baseFees = BaseFees.calcBlockFeeBreakdown(block, txrs);
-    const addressToDappMap = await getAddressToDappMap();
-    const { dappFees, unknownDappFees } = segmentBaseFeeTotalType(
-      addressToDappMap,
-      baseFees,
-    );
+    const contractAddresses = Object.keys(baseFees.contract_use_fees);
 
-    const unknownDappAddresses = unknownDappFees.map(([address]) => address);
-
-    await ensureContractAddressKnown(unknownDappAddresses);
-
-    await Promise.all([
-      updateTotalsWithFees(block, baseFees),
-      ensureFreshTotals("dapp", Object.keys(dappFees)),
-      ensureFreshTotals("contract", Object.keys(unknownDappFees)),
-    ]);
+    await ensureContractAddressKnown(contractAddresses);
+    await updateTotalsWithFees(block, baseFees);
+    await ensureFreshTotals(contractAddresses);
 
     await notifyNewLeaderboard(block);
 
