ce1213d0b5060e16bfacc679b0b21d3ebedf5e1d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-21 21:36:04 +0200

    make analyze missing blocks job exit

--- src/analyze_missing_blocks.ts
+++ src/analyze_missing_blocks.ts
@@ -1,10 +1,17 @@
 import * as BaseFees from "./base_fees.js";
 import { setName } from "./config.js";
+import { sql } from "./db.js";
 import * as Log from "./log.js";
+import * as Eth from "./web3.js";
 
 setName("analyze-missing-blocks");
 
-BaseFees.analyzeMissingBlocks().catch((error) => {
-  Log.error("error watching and analyzing new blocks", { error });
-  throw error;
-});
+BaseFees.analyzeMissingBlocks()
+  .then(async () => {
+    Eth.closeWeb3Ws();
+    await sql.end();
+  })
+  .catch((error) => {
+    Log.error("error watching and analyzing new blocks", { error });
+    throw error;
+  });
