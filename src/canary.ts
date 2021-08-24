2b2b068053ad7e8c3a15d18726ee9d641634ebf5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-24 18:32:45 +0200

    make canary log instead of throw

--- src/canary.ts
+++ src/canary.ts
@@ -1,4 +1,5 @@
 import { milisFromSeconds } from "./duration.js";
+import * as Log from "./log.js"
 
 type CanaryType = "block" | "leaderboard";
 const cage: Record<CanaryType, NodeJS.Timeout | undefined> = {
@@ -10,7 +11,7 @@ const durationMilis = milisFromSeconds(120);
 
 export const releaseCanary = (type: CanaryType): void => {
   cage[type] = setTimeout(() => {
-    throw new Error(`canary dead, no block for ${durationMilis / 1000}s`);
+    Log.error(`canary dead, no block for ${durationMilis / 1000}s`);
   }, durationMilis);
 };
 
