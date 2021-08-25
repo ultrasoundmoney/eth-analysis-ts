fb872d420880417850970d39956ce2c3fe0f5b88
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-25 12:50:33 +0200

    batch new block analysis

--- src/add_new_block_analysis.ts
+++ src/add_new_block_analysis.ts
@@ -7,8 +7,9 @@ import ProgressBar from "progress";
 import { hexToNumber } from "./hexadecimal.js";
 import PQueue from "p-queue";
 import * as Contracts from "./contracts.js";
+import A from "fp-ts/lib/Array.js";
 
-const addDataQueue = new PQueue({ concurrency: 8 });
+const buildSqlQueue = new PQueue({ concurrency: 8 });
 
 const addDataToBlocks = async (): Promise<void> => {
   const blocksMissingData = await sql<{ number: number }[]>`
@@ -18,13 +19,12 @@ const addDataToBlocks = async (): Promise<void> => {
 
   Log.info(`adding new analysis for ${blocksMissingData.length} blocks`);
 
-  const bar = new ProgressBar(":rate/s :percent :etas", {
-    renderThrottle: 3000,
-    total: blocksMissingData.length,
+  const bar = new ProgressBar("[:bar] :rate/s :percent :etas", {
+    total: blocksMissingData.length / 1000,
     stream: process.stdout,
   });
 
-  const addForBlockNumber = async (number: number): Promise<void> => {
+  const toSqlBits = async (number: number) => {
     const block = await eth.getBlock(number);
     const txrs = await Transactions.getTxrsWithRetry(block);
     const feeBreakdown = BaseFees.calcBlockFeeBreakdown(block, txrs);
@@ -32,29 +32,49 @@ const addDataToBlocks = async (): Promise<void> => {
     const ethTransferSum = feeBreakdown.transfers;
     const contractCreationSum = feeBreakdown.contract_creation_fees;
     const baseFeeSum = BaseFees.calcBlockBaseFeeSum(block);
+    const baseFeePerGas = hexToNumber(block.baseFeePerGas);
     const gasUsed = block.gasUsed;
     const addresses = Object.keys(feeBreakdown.contract_use_fees);
 
-    await Contracts.insertContracts(addresses);
-
-    await sql`
-      UPDATE base_fees_per_block
-      SET
-        tips = ${tips},
-        eth_transfer_sum = ${ethTransferSum},
-        contract_creation_sum = ${contractCreationSum},
-        base_fee_per_gas = ${hexToNumber(block.baseFeePerGas)},
-        base_fee_sum = ${baseFeeSum},
-        gas_used = ${gasUsed}
-      WHERE number = ${block.number}
-    `;
-
-    bar.tick();
+    return {
+      rowText: `(${block.number}, ${tips}, ${ethTransferSum}, ${contractCreationSum}, ${baseFeeSum}, ${baseFeePerGas}, ${gasUsed})`,
+      addresses,
+    };
   };
 
-  await addDataQueue.addAll(
-    blocksMissingData.map((number) => () => addForBlockNumber(number)),
-  );
+  for (const chunk of A.chunksOf(1000)(blocksMissingData)) {
+    const sqlBuildingBar = new ProgressBar("[:bar] :rate/s :percent :etas", {
+      total: 1000,
+    });
+    const bits = await buildSqlQueue.addAll(
+      chunk.map(
+        (number) => () =>
+          toSqlBits(number).then((result) => {
+            sqlBuildingBar.tick();
+            return result;
+          }),
+      ),
+    );
+
+    const addresses = bits.map((bit) => bit.addresses).flat();
+    await Contracts.insertContracts(addresses);
+
+    await sql.unsafe(`
+      UPDATE base_fees_per_block
+      SET
+        tips = value_list.tips,
+        eth_transfer_sum = value_list.eth_transfer_sum,
+        contract_creation_sum = value_list.contract_creation_sum,
+        base_fee_sum = value_list.base_fee_sum,
+        base_fee_per_gas = value_list.base_fee_per_gas,
+        gas_used = value_list.gas_used
+      FROM (
+        VALUES ${bits.map((bit) => bit.rowText).join(",")}
+      ) AS value_list (number, tips, eth_transfer_sum, contract_creation_sum, base_fee_sum, base_fee_per_gas, gas_used)
+      WHERE base_fees_per_block.number = value_list.number
+  `);
+    bar.tick();
+  }
 };
 
 addDataToBlocks()
