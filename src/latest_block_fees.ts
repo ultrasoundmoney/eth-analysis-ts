b053058ffd29bb2255db0fdca5624d24b39aecfd
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-08-28 23:35:57 +0200

    map latest block fees into expected format

--- src/latest_block_fees.ts
+++ src/latest_block_fees.ts
@@ -8,10 +8,13 @@ export type LatestBlockFees = LatestBlock[];
 export const getLatestBlockFees = (
   block: BlockLondon,
 ): T.Task<LatestBlockFees> => {
-  return () => sql`
+  return () =>
+    sql<{ number: number; baseFeeSum: number }[]>`
     SELECT number, base_fee_sum FROM blocks
     WHERE number <= ${block.number}
     ORDER BY (number) DESC
     LIMIT 7
-  `;
+  `.then((rows) =>
+      rows.map((row) => ({ number: row.number, fees: row.baseFeeSum })),
+    );
 };
