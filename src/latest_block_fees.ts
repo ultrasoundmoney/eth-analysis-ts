ae89f3b7b26868db2eac94e7abdd96224e7e2354
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-09-07 16:22:04 +0200

    add base fee per gas to latest block fees

--- src/latest_block_fees.ts
+++ src/latest_block_fees.ts
@@ -9,12 +9,16 @@ export const getLatestBlockFees = (
   block: BlockLondon,
 ): T.Task<LatestBlockFees> => {
   return () =>
-    sql<{ number: number; baseFeeSum: number }[]>`
-    SELECT number, base_fee_sum FROM blocks
+    sql<{ number: number; baseFeeSum: number; baseFeePerGas: number }[]>`
+    SELECT number, base_fee_sum, base_fee_per_gas FROM blocks
     WHERE number <= ${block.number}
     ORDER BY (number) DESC
     LIMIT 7
   `.then((rows) =>
-      rows.map((row) => ({ number: row.number, fees: row.baseFeeSum })),
+      rows.map((row) => ({
+        number: row.number,
+        fees: row.baseFeeSum,
+        baseFeePerGas: row.baseFeePerGas,
+      })),
     );
 };
