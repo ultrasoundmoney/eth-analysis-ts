bea7df21af7183efbf9c0404f80620324ca6674a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-11-14 11:07:58 +0100

    add milis from minutes fn

--- src/duration.ts
+++ src/duration.ts
@@ -3,3 +3,5 @@ export const milisFromSeconds = (seconds: number): number => seconds * 1000;
 export const secondsFromHours = (hours: number): number => hours * 60 * 60;
 
 export const milisFromHours = (hours: number): number => hours * 60 * 60 * 1000;
+
+export const milisFromMinutes = (minutes: number): number => minutes * 60;
