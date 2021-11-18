a769fa58c8f7c72ea638861a9bb2448a0b0abe76
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-11-18 17:06:00 +0000

    update schema

--- init.sql
+++ init.sql
@@ -11,18 +11,19 @@ CREATE TABLE "blocks" (
   "hash" text PRIMARY KEY,
   "number" int UNIQUE NOT NULL,
   "mined_at" timestamptz NOT NULL,
-  "tips" float,
-  "base_fee_sum" float,
-  "contract_creation_sum" float,
-  "eth_transfer_sum" float,
+  "tips" double,
+  "base_fee_sum" double,
+  "contract_creation_sum" double,
+  "eth_transfer_sum" double,
   "base_fee_per_gas" bigint,
-  "gas_used" bigint
+  "gas_used" bigint,
+  "eth_price" double
 );
 
 CREATE TABLE "contract_base_fees" (
   "block_number" int,
   "contract_address" text,
-  "base_fees" float,
+  "base_fees" double,
   PRIMARY KEY ("block_number", "contract_address")
 );
 
@@ -30,7 +31,6 @@ CREATE TABLE "contracts" (
   "address" text PRIMARY KEY,
   "name" text,
   "is_bot" boolean DEFAULT false,
-  "dapp_id" text,
   "category" text,
   "twitter_handle" text,
   "image_url" text,
@@ -52,12 +52,10 @@ CREATE TABLE "contracts" (
   "twitter_description" text,
   "web3_name" text,
   "web3_supports_eip_721" boolean,
-  "web3_supports_eip_1155" boolean
-);
-
-CREATE TABLE "dapps" (
-  "dapp_id" text PRIMARY KEY,
-  "name" text
+  "web3_supports_eip_1155" boolean,
+  "mined_at_block" int,
+  "mined_at" timestamptz,
+  "force_metadata_fetch" boolean
 );
 
 CREATE TABLE "derived_block_stats" (
@@ -69,7 +67,8 @@ CREATE TABLE "derived_block_stats" (
 
 CREATE TABLE "contract_base_fee_sums" (
   "contract_address" text PRIMARY KEY,
-  "base_fee_sum" float
+  "base_fee_sum" double,
+  "base_fee_sum_usd" double
 );
 
 CREATE TABLE "base_fee_sum_included_blocks" (
@@ -78,11 +77,21 @@ CREATE TABLE "base_fee_sum_included_blocks" (
   "timeframe" timeframe PRIMARY KEY
 );
 
+CREATE TABLE "contract_creations" (
+  "address" text,
+  "block_number" int
+);
+
+CREATE TABLE "eth_prices" (
+  "timestamp" timestamptz PRIMARY KEY,
+  "ethusd" double
+);
+
 ALTER TABLE "contract_base_fees" ADD FOREIGN KEY ("block_number") REFERENCES "blocks" ("number");
 
 ALTER TABLE "contract_base_fees" ADD FOREIGN KEY ("contract_address") REFERENCES "contracts" ("address");
 
-ALTER TABLE "contracts" ADD FOREIGN KEY ("dapp_id") REFERENCES "dapps" ("dapp_id");
+ALTER TABLE "contracts" ADD FOREIGN KEY ("mined_at_block") REFERENCES "blocks" ("number");
 
 ALTER TABLE "derived_block_stats" ADD FOREIGN KEY ("block_number") REFERENCES "blocks" ("number");
 
@@ -95,3 +104,5 @@ ALTER TABLE "base_fee_sum_included_blocks" ADD FOREIGN KEY ("newest_included_blo
 CREATE INDEX ON "blocks" ("number");
 
 CREATE INDEX ON "blocks" ("mined_at");
+
+CREATE INDEX ON "contracts" ("force_metadata_fetch");
