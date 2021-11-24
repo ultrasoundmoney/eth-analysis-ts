1c020b4460af4c197e16bbf9a15e8ca3de7c2a9c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-11-24 19:54:28 +0100

    add types to hex conversions

--- src/hexadecimal.ts
+++ src/hexadecimal.ts
@@ -1,3 +1,3 @@
-export const hexToNumber = (hex: string) => Number.parseInt(hex, 16);
+export const hexToNumber = (hex: string): number => Number.parseInt(hex, 16);
 
-export const numberToHex = (num: number) => `0x${num.toString(16)}`;
+export const numberToHex = (num: number): string => `0x${num.toString(16)}`;
