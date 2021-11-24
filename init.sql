aaca46b46a20d5a498c3e30b8909dc4022496b3d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-11-24 22:21:06 +0100

    sort sql schema columns

--- init.sql
+++ init.sql
@@ -8,54 +8,54 @@ CREATE TYPE "timeframe" AS ENUM (
 );
 
 CREATE TABLE "blocks" (
-  "hash" text PRIMARY KEY,
-  "number" int UNIQUE NOT NULL,
-  "mined_at" timestamptz NOT NULL,
-  "tips" double precision,
+  "base_fee_per_gas" bigint,
   "base_fee_sum" double precision,
   "contract_creation_sum" double precision,
+  "eth_price" double precision,
   "eth_transfer_sum" double precision,
-  "base_fee_per_gas" bigint,
   "gas_used" bigint,
-  "eth_price" double precision
+  "hash" text PRIMARY KEY,
+  "mined_at" timestamptz NOT NULL,
+  "number" int UNIQUE NOT NULL,
+  "tips" double precision
 );
 
 CREATE TABLE "contract_base_fees" (
+  "base_fees" double precision,
   "block_number" int,
   "contract_address" text,
-  "base_fees" double precision,
   PRIMARY KEY ("block_number", "contract_address")
 );
 
 CREATE TABLE "contracts" (
   "address" text PRIMARY KEY,
-  "name" text,
-  "is_bot" boolean DEFAULT false,
   "category" text,
-  "twitter_handle" text,
-  "image_url" text,
+  "defi_llama_category" text,
+  "defi_llama_twitter_handle" text,
   "etherscan_name_tag" text,
   "etherscan_name_token" text,
-  "opensea_contract_last_fetch" timestamptz,
-  "opensea_name" text,
-  "opensea_twitter_handle" text,
-  "opensea_category" text,
-  "opensea_schema_name" text,
-  "opensea_image_url" text,
-  "defi_llama_twitter_handle" text,
-  "defi_llama_category" text,
+  "force_metadata_fetch" boolean,
+  "image_url" text,
+  "is_bot" boolean DEFAULT false,
+  "manual_category" text,
   "manual_name" text,
   "manual_twitter_handle" text,
-  "manual_category" text,
+  "mined_at" timestamptz,
+  "mined_at_block" int,
+  "name" text,
+  "opensea_category" text,
+  "opensea_contract_last_fetch" timestamptz,
+  "opensea_image_url" text,
+  "opensea_name" text,
+  "opensea_schema_name" text,
+  "opensea_twitter_handle" text,
+  "twitter_description" text,
+  "twitter_handle" text,
   "twitter_image_url" text,
   "twitter_name" text,
-  "twitter_description" text,
   "web3_name" text,
-  "web3_supports_eip_721" boolean,
   "web3_supports_eip_1155" boolean,
-  "mined_at_block" int,
-  "mined_at" timestamptz,
-  "force_metadata_fetch" boolean
+  "web3_supports_eip_721" boolean
 );
 
 CREATE TABLE "derived_block_stats" (
@@ -66,14 +66,14 @@ CREATE TABLE "derived_block_stats" (
 );
 
 CREATE TABLE "contract_base_fee_sums" (
-  "contract_address" text PRIMARY KEY,
   "base_fee_sum" double precision,
-  "base_fee_sum_usd" double precision
+  "base_fee_sum_usd" double precision,
+  "contract_address" text PRIMARY KEY
 );
 
 CREATE TABLE "base_fee_sum_included_blocks" (
-  "oldest_included_block" int,
   "newest_included_block" int,
+  "oldest_included_block" int,
   "timeframe" timeframe PRIMARY KEY
 );
 
@@ -83,17 +83,17 @@ CREATE TABLE "contract_creations" (
 );
 
 CREATE TABLE "eth_prices" (
-  "timestamp" timestamptz PRIMARY KEY,
   "ethusd" double precision,
   "ethusd_24h_change" double precision
+  "timestamp" timestamptz PRIMARY KEY,
 );
 
 CREATE TABLE "market_caps" (
-  "timestamp" timestamptz PRIMARY KEY,
   "btc_market_cap" double precision,
   "eth_market_cap" double precision,
   "gold_market_cap" double precision,
-  "usd_m2_market_cap" double precision
+  "timestamp" timestamptz PRIMARY KEY,
+  "usd_m3_market_cap" double precision
 );
 
 ALTER TABLE "contract_base_fees" ADD FOREIGN KEY ("block_number") REFERENCES "blocks" ("number");
