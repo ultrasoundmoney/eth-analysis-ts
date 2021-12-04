f238e69fe909c9883c4b193f2335e621e1088dac
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-03 18:06:32 -0600

    export more fp helpers

--- src/fp.ts
+++ src/fp.ts
@@ -1,41 +1,72 @@
-import { sequenceS, sequenceT } from "fp-ts/lib/Apply.js";
+import * as Apply from "fp-ts/lib/Apply.js";
 import { pipe } from "fp-ts/lib/function.js";
+import * as Mo from "fp-ts/lib/Monoid.js";
 import * as T from "fp-ts/lib/Task.js";
 import * as TE from "fp-ts/lib/TaskEither.js";
+import * as Void from "fp-ts/lib/void.js";
 
 export * as A from "fp-ts/lib/Array.js";
 export * as B from "fp-ts/lib/boolean.js";
 export * as E from "fp-ts/lib/Either.js";
 export { flow, pipe } from "fp-ts/lib/function.js";
+export * as IO from "fp-ts/lib/IO.js";
+export * as Mo from "fp-ts/lib/Monoid.js";
 export * as NEA from "fp-ts/lib/NonEmptyArray.js";
 export * as Num from "fp-ts/lib/number.js";
 export * as O from "fp-ts/lib/Option.js";
-export * as Ord from "fp-ts/lib/Ord.js";
+export { Ord } from "fp-ts/lib/Ord.js";
+export * as OrdM from "fp-ts/lib/Ord.js";
 export * as RTE from "fp-ts/lib/ReaderTaskEither.js";
 export * as RA from "fp-ts/lib/ReadonlyArray.js";
 export * as T from "fp-ts/lib/Task.js";
 export * as TE from "fp-ts/lib/TaskEither.js";
+export * as Void from "fp-ts/lib/void.js";
 export * as D from "io-ts/lib/Decoder.js";
 
-const getOrThrow = <A>(te: TE.TaskEither<string | undefined, A>): T.Task<A> =>
+type ErrorLike = { error: Error };
+
+const getOrThrow = <A>(
+  te: TE.TaskEither<string | Error | ErrorLike, A>,
+): T.Task<A> =>
   pipe(
     te,
-    TE.getOrElse((e) => {
-      throw new Error(e);
+    TE.getOrElse((e): never => {
+      if (e instanceof Error) {
+        throw e;
+      }
+
+      if (typeof e === "string") {
+        throw new Error(e);
+      }
+
+      if ("error" in e && e.error instanceof Error) {
+        throw e.error;
+      }
+
+      throw new Error("getOrThrow, failed to throw error");
     }),
   );
 
 export const TAlt = {
-  seqTParT: sequenceT(T.ApplyPar),
-  seqTSeqT: sequenceT(T.ApplySeq),
-  seqSParT: sequenceS(T.ApplyPar),
-  seqSSeqT: sequenceS(T.ApplySeq),
+  concatAllVoid: T.map(Mo.concatAll(Void.Monoid)),
+  seqSParT: Apply.sequenceS(T.ApplyPar),
+  seqSSeqT: Apply.sequenceS(T.ApplySeq),
+  seqTParT: Apply.sequenceT(T.ApplyPar),
+  seqTSeqT: Apply.sequenceT(T.ApplySeq),
 };
 
 export const TEAlt = {
-  seqTParTE: sequenceT(TE.ApplyPar),
-  seqTSeqTE: sequenceT(TE.ApplySeq),
-  seqSParTE: sequenceS(TE.ApplyPar),
-  seqSSeqTE: sequenceS(TE.ApplySeq),
+  concatAllVoid: TE.map(Mo.concatAll(Void.Monoid)),
   getOrThrow,
+  seqSParTE: Apply.sequenceS(TE.ApplyPar),
+  seqSSeqTE: Apply.sequenceS(TE.ApplySeq),
+  seqTParTE: Apply.sequenceT(TE.ApplyPar),
+  seqTSeqTE: Apply.sequenceT(TE.ApplySeq),
+  unknownToError: (e: unknown): Error => {
+    if (e instanceof Error) {
+      return e;
+    }
+
+    return new Error(String(e));
+  },
 };
