b58beecb4c24bbd67c154dee9e32b21bcf48b9d1
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-11 01:47:46 -0600

    add json helper

---
+++ src/json.ts
@@ -0,0 +1,9 @@
+export const serialize = (_key: unknown, value: unknown) =>
+  typeof value === "bigint" ? value.toString() + "n" : value;
+
+export const deserialize = (_key: string, value: unknown) => {
+  if (typeof value === "string" && /^\d+n$/.test(value)) {
+    return BigInt(value.slice(0, -1));
+  }
+  return value;
+};
