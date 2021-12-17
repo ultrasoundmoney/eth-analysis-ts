5e1be3367ba4f6ceb81b949f9309bce6f07d6b77
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-17 10:53:47 -0600

    expand scarcity to work like other derived stats

--- src/scarcity/new_head.ts
+++ src/scarcity/new_head.ts
@@ -1,35 +1,12 @@
 import { BlockDb } from "../blocks/blocks.js";
 import * as DateFnsAlt from "../date_fns_alt.js";
-import { sql } from "../db.js";
 import * as Duration from "../duration.js";
 import * as FeeBurn from "../fee_burns.js";
-import { serializeBigInt } from "../json.js";
 import * as Log from "../log.js";
 import * as EthLocked from "./eth_locked.js";
 import * as EthStaked from "./eth_staked.js";
 import * as EthSupply from "./eth_supply.js";
-
-export type Scarcity = {
-  engines: {
-    burned: {
-      amount: bigint;
-      name: string;
-      startedOn: Date;
-    };
-    locked: {
-      amount: number;
-      name: string;
-      startedOn: Date;
-    };
-    staked: {
-      amount: bigint;
-      name: string;
-      startedOn: Date;
-    };
-  };
-  ethSupply: bigint;
-  number: number;
-};
+import { Scarcity } from "./scarcity.js";
 
 export const onNewBlock = async (block: BlockDb) => {
   const ethBurned = FeeBurn.getAllFeesBurned().eth;
@@ -100,18 +77,7 @@ export const onNewBlock = async (block: BlockDb) => {
     number: block.number,
   };
 
-  const insertable = JSON.stringify(scarcity, serializeBigInt);
-
-  await sql`
-    INSERT INTO derived_block_stats (
-      block_number,
-      scarcity
-    ) VALUES (
-      ${block.number},
-      ${insertable}::jsonb
-    ) ON CONFLICT (block_number) DO UPDATE
-    SET scarcity = excluded.scarcity
-  `;
-
   Log.debug("done updating scarcity");
+
+  return scarcity;
 };
--- src/scarcity/scarcity.ts
+++ src/scarcity/scarcity.ts
@@ -1,15 +1,49 @@
 import { sql } from "../db.js";
+import { O, pipe } from "../fp.js";
+
+export type Scarcity = {
+  engines: {
+    burned: {
+      amount: bigint;
+      name: string;
+      startedOn: Date;
+    };
+    locked: {
+      amount: number;
+      name: string;
+      startedOn: Date;
+    };
+    staked: {
+      amount: bigint;
+      name: string;
+      startedOn: Date;
+    };
+  };
+  ethSupply: bigint;
+  number: number;
+};
+
+let lastScarcity: Scarcity | undefined = undefined;
+
+export const updateScarcity = (scarcity: Scarcity) => {
+  lastScarcity = scarcity;
+};
+
+export const getLastScarcity = (): O.Option<Scarcity> =>
+  pipe(lastScarcity, O.fromNullable);
 
 export type ScarcityJson = string;
 
-export const getLatestScarcity = async (): Promise<ScarcityJson> => {
+export const getLastStoredScarcity = async (): Promise<ScarcityJson> => {
   const rows = await sql<{ scarcity: string }[]>`
     SELECT scarcity FROM derived_block_stats
     WHERE block_number = (
       SELECT MAX(block_number) FROM derived_block_stats
+      WHERE scarcity IS NOT NULL
     )
-    AND scarcity IS NOT NULL
   `;
+
+  console.log(rows[0]?.scarcity);
 
   return rows[0]?.scarcity;
 };
