e7adfe0c5c41ab444f9b777f600eb1307bc94fdd
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-21 11:53:26 -0600

    add generic fetch with retry

--- src/fetch_alt.ts
+++ src/fetch_alt.ts
@@ -1,12 +1,20 @@
 import fetch, { RequestInfo, RequestInit, Response } from "node-fetch";
+import * as Retry from "retry-ts";
+import { retrying } from "retry-ts/lib/Task.js";
 import { delay } from "./delay.js";
+import { E, pipe, TE } from "./fp.js";
 import * as Log from "./log.js";
+
+type FetchFn = (
+  url: RequestInfo,
+  init?: RequestInit | undefined,
+) => Promise<Response>;
 
 export const withRetry = (
   limit = 3,
   delayMillis = 2000,
   useBinaryExponentialBackoff = true,
-) => {
+): FetchFn => {
   let attempt = 1;
   return async (url: RequestInfo, init?: RequestInit): Promise<Response> => {
     let res = await fetch(url, init);
@@ -39,3 +47,38 @@ export const withRetry = (
     return res;
   };
 };
+
+export class FetchError extends Error {}
+
+export const fetchWithRetry = (
+  url: RequestInfo,
+  init?: RequestInit,
+): TE.TaskEither<Error, Response> =>
+  retrying(
+    Retry.Monoid.concat(Retry.exponentialBackoff(2000), Retry.limitRetries(3)),
+    (status) =>
+      pipe(
+        TE.tryCatch(
+          () => fetch(url, init),
+          (e) => {
+            if (e instanceof Error) {
+              return e;
+            }
+
+            return new FetchError(String(e));
+          },
+        ),
+        TE.chain((res) => {
+          if (res.status >= 200 && res.status < 300) {
+            return TE.right(res);
+          }
+
+          Log.debug(
+            `fetch ${url} failed, status: ${res.status}, attempt: ${status.iterNumber}, wait sum: ${status.cumulativeDelay}ms, retrying`,
+          );
+
+          return TE.left(new Error("bad response"));
+        }),
+      ),
+    E.isLeft,
+  );
