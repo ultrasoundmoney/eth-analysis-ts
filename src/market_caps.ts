01635cb2a18b5b77d5d160c7345e6297106f9ab2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2021-12-23 12:50:44 -0600

    simplify storing of market caps

--- src/market_caps.ts
+++ src/market_caps.ts
@@ -1,10 +1,8 @@
 import * as Coingecko from "./coingecko.js";
-import { MarketDataError, PriceResponse } from "./coingecko.js";
 import { sql } from "./db.js";
 import * as Duration from "./duration.js";
-import { EthPrice } from "./etherscan.js";
 import * as EthPrices from "./eth_prices.js";
-import { pipe, T, TE, TEAlt } from "./fp.js";
+import { E, pipe, T, TE, TEAlt } from "./fp.js";
 import * as Log from "./log.js";
 
 export type MarketCaps = {
@@ -37,8 +35,6 @@ const trimMarketCapsTable = (): T.Task<void> =>
     T.map(() => undefined),
   );
 
-type StoreMarketCapsError = MarketDataError | Error | string;
-
 type MarketCapRow = {
   btc_market_cap: number;
   eth_market_cap: number;
@@ -47,55 +43,59 @@ type MarketCapRow = {
   timestamp: Date;
 };
 
+const insertableFromCoinData = (
+  coins: Coingecko.PriceResponse,
+  ethPrice: EthPrices.EthPrice,
+): MarketCapRow => {
+  const btcMarketCap = coins.bitcoin.usd_market_cap;
+  const coingeckoMarketCap = coins.ethereum.usd_market_cap;
+  const coingeckoEthPrice = coins.ethereum.usd;
+  const ethCirculatingSupply = coingeckoMarketCap / coingeckoEthPrice;
+  const ethMarketCap = ethCirculatingSupply * ethPrice.ethusd;
+  // See: https://www.gold.org/goldhub/data/above-ground-stocks which many appear to use.
+  // In tonnes.
+  const goldCirculatingSupply = 201296.1;
+  const kgPerTonne = 1000;
+  const troyOzPerKg = 1000 / 31.1034768;
+  const goldPricePerTroyOz = coins["tether-gold"].usd;
+  const goldMarketCap =
+    goldCirculatingSupply * kgPerTonne * troyOzPerKg * goldPricePerTroyOz;
+  // See: https://ycharts.com/indicators/us_m3_money_supply
+  const usdM3MarketCap = 20_982_900_000_000;
+
+  return {
+    btc_market_cap: btcMarketCap,
+    eth_market_cap: ethMarketCap,
+    gold_market_cap: goldMarketCap,
+    usd_m3_market_cap: usdM3MarketCap,
+    timestamp: new Date(),
+  };
+};
+
 export const storeCurrentMarketCaps = (): TE.TaskEither<
-  StoreMarketCapsError,
+  Coingecko.CoinGeckoApiError,
   void
-> => {
-  const getPricesTask: TE.TaskEither<StoreMarketCapsError, PriceResponse> =
-    Coingecko.getSimpleCoins();
-
-  const getEthPriceTask: TE.TaskEither<StoreMarketCapsError, EthPrice> = pipe(
-    EthPrices.getEthPrice(new Date(), Duration.millisFromMinutes(5)),
-  );
-
-  return pipe(
-    TEAlt.seqTParTE(getPricesTask, getEthPriceTask),
-    TE.map(([coins, ethPrice]): MarketCapRow => {
-      const btcMarketCap = coins.bitcoin.usd_market_cap;
-      const coingeckoMarketCap = coins.ethereum.usd_market_cap;
-      const coingeckoEthPrice = coins.ethereum.usd;
-      const ethCirculatingSupply = coingeckoMarketCap / coingeckoEthPrice;
-      const ethMarketCap = ethCirculatingSupply * ethPrice.ethusd;
-      // See: https://www.gold.org/goldhub/data/above-ground-stocks which many appear to use.
-      // In tonnes.
-      const goldCirculatingSupply = 201296.1;
-      const kgPerTonne = 1000;
-      const troyOzPerKg = 1000 / 31.1034768;
-      const goldPricePerTroyOz = coins["tether-gold"].usd;
-      const goldMarketCap =
-        goldCirculatingSupply * kgPerTonne * troyOzPerKg * goldPricePerTroyOz;
-      // See: https://ycharts.com/indicators/us_m3_money_supply
-      const usdM3MarketCap = 20_982_900_000_000;
-
-      return {
-        btc_market_cap: btcMarketCap,
-        eth_market_cap: ethMarketCap,
-        gold_market_cap: goldMarketCap,
-        usd_m3_market_cap: usdM3MarketCap,
-        timestamp: new Date(),
-      };
-    }),
-    TE.chainFirstTaskK(storeMarketCaps),
-    // Don't let the table grow too much.
-    TE.chainFirstTaskK(trimMarketCapsTable),
+> =>
+  pipe(
+    TEAlt.seqTParTE(
+      Coingecko.getSimpleCoins(),
+      EthPrices.getEthPrice(new Date(), Duration.millisFromMinutes(5)),
+    ),
+    TE.map(
+      ([coins, ethPrice]): MarketCapRow =>
+        insertableFromCoinData(coins, ethPrice),
+    ),
+    TE.chainW((marketCaps) =>
+      pipe(storeMarketCaps(marketCaps), T.map(E.right)),
+    ),
+    // // Don't let the table grow too much.
+    TE.chainW(() => pipe(trimMarketCapsTable(), T.map(E.right))),
     TE.chainFirstIOK(() => () => {
       Log.debug(`stored market caps at ${new Date().toISOString()}`);
     }),
-    TE.map(() => undefined),
   );
-};
 
-export const getMarketCaps = async (): Promise<MarketCaps> =>
+export const getStoredMarketCaps = async (): Promise<MarketCaps> =>
   sql<MarketCaps[]>`
     SELECT
       btc_market_cap,
