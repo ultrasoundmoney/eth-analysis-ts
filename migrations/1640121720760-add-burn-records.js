0eeede09838b9621fb1e3d218d2bea15958f2d79
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-04 20:53:05 -0500

    add unique constraint for records

--- migrations/1640121720760-add-burn-records.js
+++ migrations/1640121720760-add-burn-records.js
@@ -4,7 +4,8 @@ export async function up(client) {
       "burn_record_id" serial PRIMARY KEY,
       "time_frame" text NOT NULL,
       "block_number" int NOT NULL,
-      "base_fee_sum" float8 NOT NULL
+      "base_fee_sum" float8 NOT NULL,
+      UNIQUE (time_frame, block_number)
     )
   `;
 
