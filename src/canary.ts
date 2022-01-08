550b1fbc484db8827ab1a2d2b7a6614cd1f2f75d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-07 19:20:01 -0600

    use alert log for canary

--- src/canary.ts
+++ src/canary.ts
@@ -1,4 +1,5 @@
-import { millisFromSeconds } from "./duration.js";
+import * as Duration from "./duration.js";
+import * as Log from "./log.js";
 
 type CanaryType = "block" | "leaderboard";
 const cage: Record<CanaryType, NodeJS.Timeout | undefined> = {
@@ -6,11 +7,11 @@ const cage: Record<CanaryType, NodeJS.Timeout | undefined> = {
   leaderboard: undefined,
 };
 
-const durationMilis = millisFromSeconds(120);
+const durationMilis = Duration.millisFromSeconds(120);
 
 export const releaseCanary = (type: CanaryType): void => {
   cage[type] = setTimeout(() => {
-    throw new Error(`canary dead, no block for ${durationMilis / 1000}s`);
+    Log.alert(`canary dead, no block for ${durationMilis / 1000}s`);
   }, durationMilis);
 };
 
