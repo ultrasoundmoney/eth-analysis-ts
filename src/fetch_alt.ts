c1779bb9a9be227d9140d33d3c52731dd38ca247
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-11 11:35:02 -0600

    remove unused fetch helper

--- src/fetch_alt.ts
+++ src/fetch_alt.ts
@@ -4,49 +4,6 @@ import { retrying } from "retry-ts/lib/Task.js";
 import { E, pipe, TE } from "./fp.js";
 import * as Log from "./log.js";
 
-type FetchFn = (
-  url: RequestInfo,
-  init?: RequestInit | undefined,
-) => Promise<Response>;
-
-export const withRetry = (
-  limit = 3,
-  delayMillis = 2000,
-  useBinaryExponentialBackoff = true,
-): FetchFn => {
-  let attempt = 1;
-  return async (url: RequestInfo, init?: RequestInit): Promise<Response> => {
-    let res = await fetch(url, init);
-
-    const delayMultiplier = useBinaryExponentialBackoff
-      ? 2 ** (attempt - 1)
-      : 1;
-    const nextDelay = delayMillis * delayMultiplier;
-
-    while (attempt !== limit) {
-      if (res.status >= 200 && res.status < 300) {
-        return res;
-      }
-
-      Log.debug(
-        `fetch ${url} failed, status: ${res.status}, attempt: ${attempt}, retrying`,
-      );
-
-      Log.debug(`waiting ${nextDelay / 1000}s before retry`);
-      await delay(nextDelay);
-
-      attempt = attempt + 1;
-      res = await fetch(url, init);
-    }
-
-    Log.debug(
-      `fetch ${url} failed, hit retry limit, returning response as is.`,
-    );
-
-    return res;
-  };
-};
-
 export class FetchError extends Error {}
 export class BadResponseError extends Error {
   public status: number;
