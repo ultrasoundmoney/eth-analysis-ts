86301de9bb19f55aedbc22c413c3ca8b7c6a0930
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-11 21:23:23 -0600

    split ftx price fetching
    We support getting a range, and getting a price for a specific date now.

--- src/eth_prices_ftx.ts
+++ src/eth_prices_ftx.ts
@@ -1,13 +1,11 @@
 import * as DateFns from "date-fns";
-import fetch from "node-fetch";
 import PQueue from "p-queue";
+import * as Retry from "retry-ts";
 import urlcatM from "urlcat";
-import * as DateFnsAlt from "./date_fns_alt.js";
 import { JsTimestamp } from "./date_fns_alt.js";
 import * as Duration from "./duration.js";
-import { EthPrice, HistoricPrice } from "./eth_prices.js";
-import { A, pipe } from "./fp.js";
-import * as Log from "./log.js";
+import * as FetchAlt from "./fetch_alt.js";
+import { A, E, O, pipe, T, TE } from "./fp.js";
 
 // NOTE: import is broken somehow, "urlcat is not a function" without.
 const urlcat = (urlcatM as unknown as { default: typeof urlcatM }).default;
@@ -22,96 +20,104 @@ type IndexPriceResponse = {
   success: boolean;
 };
 
-// FTX says they allow 6 requests per second. We're not sure yet.
+type HistoricPriceMap = Map<JsTimestamp, number>;
+
+// FTX says they allow 6 requests per second. Haven't tested this limit.
 export const ftxApiQueue = new PQueue({
   concurrency: 2,
   interval: Duration.millisFromSeconds(1),
   intervalCap: 3,
 });
 
-export const getFtxPrices = async (
-  earlierMinutesToFetch: number,
-  timestamp: Date,
-): Promise<HistoricPrice[]> => {
-  if (earlierMinutesToFetch > 1500) {
-    throw new Error("cannot fetch more than 1500 minutes at a time");
-  }
+const queueApiCall =
+  <A>(task: T.Task<A>): T.Task<A> =>
+  () =>
+    ftxApiQueue.add(task);
 
-  const startTime = pipe(
-    timestamp,
-    DateFns.startOfMinute,
-    // FTX returns up to 1500 results per page. We do not support pagination and so cannot return prices for more than 1500 minutes at a time.
-    (dt) => DateFns.subMinutes(dt, earlierMinutesToFetch),
-    DateFns.getUnixTime,
-  );
-  const endTime = pipe(timestamp, DateFns.startOfMinute, DateFns.getUnixTime);
-
-  const url = urlcat("https://ftx.com/api/indexes/ETH/candles", {
+const makeEthPriceUrl = (start: Date, end: Date) =>
+  urlcat("https://ftx.com/api/indexes/ETH/candles", {
     resolution: 60,
-    start_time: startTime,
-    end_time: endTime,
+    start_time: DateFns.getUnixTime(start),
+    end_time: DateFns.getUnixTime(end),
   });
 
-  const res = await ftxApiQueue.add(() => fetch(url));
+const retryPolicy = Retry.Monoid.concat(
+  Retry.constantDelay(2000),
+  Retry.limitRetries(2),
+);
 
-  if (res.status !== 200) {
-    Log.error("failed to fetch ftx price", await res.json());
-    throw new Error(`failed to fetch ftx prices, status: ${res.status}`);
+export const getFtxPrices = (startDateTime: Date, endDateTime: Date) => {
+  const startMinute = DateFns.startOfMinute(startDateTime);
+  const endMinute = DateFns.startOfMinute(endDateTime);
+  const minutesTotal = DateFns.differenceInMinutes(endMinute, startMinute);
+
+  // FTX returns up to 1500 results per page. We do not support pagination and so cannot return prices for more than 1500 minutes at a time.
+  if (minutesTotal > 1500) {
+    return TE.left(
+      new Error(
+        `tried to fetch more prices in one batch (${minutesTotal}) than allowed (1500) from FTX`,
+      ),
+    );
   }
 
-  const pricesResponse = (await res.json()) as IndexPriceResponse;
-  const prices = pricesResponse.result;
-
   return pipe(
-    prices,
-    A.map((indexPrice) => [indexPrice.time, indexPrice.open]),
+    FetchAlt.fetchWithRetry(
+      makeEthPriceUrl(startMinute, endMinute),
+      undefined,
+      {
+        retryPolicy,
+      },
+    ),
+    queueApiCall,
+    TE.chainW((res) =>
+      pipe(() => res.json() as Promise<IndexPriceResponse>, T.map(E.right)),
+    ),
+    TE.map(
+      (body): HistoricPriceMap =>
+        pipe(
+          body.result,
+          A.reduce(new Map(), (map, indexPrice) =>
+            map.set(indexPrice.time, indexPrice.open),
+          ),
+        ),
+    ),
   );
 };
 
-const findNearestHistoricPrice = (
-  orderedPrices: HistoricPrice[],
-  target: Date | number,
-): HistoricPrice => {
-  let nearestPrice = orderedPrices[0];
+export class PriceNotFound extends Error {}
 
-  for (const price of orderedPrices) {
-    const distanceCandidate = Math.abs(
-      DateFns.differenceInSeconds(target, price[0]),
-    );
-    const distanceCurrent = Math.abs(
-      DateFns.differenceInSeconds(target, nearestPrice[0]),
-    );
-
-    // Prices are ordered from oldest to youngest. If the next candidate is further away, the target has to be older. As coming options are only ever younger, we can stop searching.
-    if (distanceCandidate > distanceCurrent) {
-      break;
-    }
-
-    nearestPrice = price;
-    continue;
-  }
-
-  return nearestPrice;
-};
-
-export const getNearestFtxPrice = async (
-  maxDistanceInSeconds: number,
-  timestamp: Date,
-): Promise<EthPrice | undefined> => {
-  const prices = await getFtxPrices(2, timestamp);
-  const nearestPrice = findNearestHistoricPrice(prices, timestamp);
-  Log.debug("ftx nearest", { prices, blockMinedAt: timestamp, nearestPrice });
-  const distance = DateFnsAlt.secondsBetweenAbs(nearestPrice[0], timestamp);
-
-  if (distance > maxDistanceInSeconds) {
-    Log.warn(`nearest ftx price not close enough, diff: ${distance}s`);
-    return undefined;
-  }
-
-  Log.debug(`found a close enough ftx price, diff: ${distance}s`);
-
-  return {
-    timestamp: new Date(nearestPrice[0]),
-    ethusd: nearestPrice[1],
-  };
-};
+export const getPriceByDate = (dt: Date) =>
+  pipe(
+    {
+      start: pipe(dt, DateFns.startOfMinute, (dt) => DateFns.subMinutes(dt, 1)),
+      end: DateFns.addMinutes(dt, 1),
+    },
+    ({ start, end }) =>
+      pipe(
+        getFtxPrices(start, end),
+        TE.chainW((prices) =>
+          pipe(
+            prices.get(DateFns.getTime(start)),
+            O.fromNullable,
+            // Sometimes the start of the current minute is too recent for FTX to have a price, so we ask for the past two minutes and return the most recent.
+            O.alt(() =>
+              pipe(
+                prices.get(DateFns.getTime(DateFns.subMinutes(start, 1))),
+                O.fromNullable,
+              ),
+            ),
+            E.fromOption(
+              () =>
+                new PriceNotFound(
+                  `FTX returned prices from ${start}, to ${end}, but price for requested date is missing`,
+                ),
+            ),
+            TE.fromEither,
+          ),
+        ),
+        TE.map((price) => ({
+          timestamp: start,
+          ethusd: price,
+        })),
+      ),
+  );
