b74215302bb03eaeda2d5edb0fbf3205ba916e49
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-01-11 23:35:57 -0600

    remove high watermark monitoring

--- src/store_eth_price.ts
+++ src/store_eth_price.ts
@@ -1,34 +1,18 @@
-import * as DateFns from "date-fns";
 import { setInterval } from "timers/promises";
 import * as Duration from "./duration.js";
 import * as EthPrices from "./eth-prices/eth_prices.js";
 import { pipe, TEAlt } from "./fp.js";
-import * as Log from "./log.js";
 
 process.on("unhandledRejection", (error) => {
   throw error;
 });
-
-const watermark = 30;
 
 const intervalIterator = setInterval(
   Duration.millisFromSeconds(10),
   Date.now(),
 );
 
-let lastRun = new Date();
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 for await (const _ of intervalIterator) {
-  const secondsSinceLastRun = DateFns.differenceInSeconds(new Date(), lastRun);
-
-  if (secondsSinceLastRun >= watermark) {
-    Log.error(
-      `store price not keeping up, ${secondsSinceLastRun}s since last price fetch`,
-    );
-  }
-
-  lastRun = new Date();
-
   await pipe(EthPrices.storeCurrentEthPrice(), TEAlt.getOrThrow)();
 }
