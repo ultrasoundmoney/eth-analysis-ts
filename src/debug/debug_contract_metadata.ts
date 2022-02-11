d7493d1065767db9a1e703f32e47f1ede54fc5b2
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-11 22:56:37 +0100

    fix: remove missed connect call

--- src/debug/debug_contract_metadata.ts
+++ src/debug/debug_contract_metadata.ts
@@ -6,7 +6,6 @@ import * as Log from "../log.js";
 const main = async () => {
   try {
     Log.info("starting add-contract-metadata");
-    await EthNode.connect();
 
     await ContractsMetadata.addMetadataForLeaderboards(
       ["0x59c0e4b889f4c036dd0d6d759c7b37cf91f3ec01"],
