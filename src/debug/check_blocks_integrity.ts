cc0ecb5466e14c766823af0f0d262ac6e7e8b6ab
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-11 14:38:24 +0100

    feat: check for gas and fee integrity

--- src/debug/check_blocks_integrity.ts
+++ src/debug/check_blocks_integrity.ts
@@ -1,9 +1,24 @@
+import makeEta from "simple-eta";
 import { sql } from "../db.js";
 import * as EthNode from "../eth_node.js";
+import { hexFromNumber } from "../hexadecimal.js";
 import * as Log from "../log.js";
 
-await sql<{ hash: string; number: number }[]>`
-  SELECT hash, number FROM blocks
+const left = await sql`SELECT MIN(number) FROM blocks`.then(
+  (rows) => rows[0].min,
+);
+const right = await sql`SELECT MAX(number) FROM blocks`.then(
+  (rows) => rows[0].max,
+);
+const blocksToScan = right - left + 1;
+let blocksDone = 0;
+const eta = makeEta({ max: blocksToScan });
+
+await sql<
+  { hash: string; number: number; baseFeePerGas: string; gasUsed: string }[]
+>`
+  SELECT hash, number, base_fee_per_gas, gas_used FROM blocks
+  ORDER By number ASC
 `.cursor(1000, async (rows) => {
   for (const row of rows) {
     const block = await EthNode.getBlock(row.number);
@@ -12,6 +27,28 @@ await sql<{ hash: string; number: number }[]>`
         `found bad block ${row.number}, our hash: ${row.hash}, their hash: ${block?.hash}`,
       );
     }
+
+    if (
+      hexFromNumber(Number(row.baseFeePerGas)) !== block.baseFeePerGas ||
+      hexFromNumber(Number(row.gasUsed)) !== block.gasUsed
+    ) {
+      Log.debug("found bad block", {
+        row,
+        block: {
+          ...block,
+          transactions: null,
+          baseFeePerGasNum: Number(block.baseFeePerGas),
+          gasUsedNum: Number(block.gasUsed),
+        },
+      });
+      throw new Error("hit bad block");
+    }
   }
-  Log.debug("1000 blocks checked..");
+
+  blocksDone = blocksDone + 1000;
+  eta.report(blocksDone);
+  eta.estimate();
+  Log.debug(
+    `blocks scanned: ${blocksDone}, eta: ${eta.estimate().toFixed(0)}s`,
+  );
 });
