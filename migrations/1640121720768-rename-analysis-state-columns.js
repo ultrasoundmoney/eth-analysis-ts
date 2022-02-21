5c275ad0f6690e95c01a88a7efd84f450cd94a66
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-21 21:54:00 +0100

    fix: migration table name

--- migrations/1640121720768-rename-analysis-state-columns.js
+++ migrations/1640121720768-rename-analysis-state-columns.js
@@ -1,21 +1,21 @@
 export async function up(client) {
   await client`
-    ALTER TABLE analysis-state
+    ALTER TABLE analysis_state
       RENAME COLUMN first_analyzed_block TO first
   `;
   await client`
-    ALTER TABLE analysis-state
+    ALTER TABLE analysis_state
       RENAME COLUMN last_analyzed_block TO last
   `;
 }
 
 export async function down(client) {
   await client`
-    ALTER TABLE analysis-state
+    ALTER TABLE analysis_state
       RENAME COLUMN first TO first_analyzed_block
   `;
   await client`
-    ALTER TABLE analysis-state
+    ALTER TABLE analysis_state
       RENAME COLUMN last TO last_analyzed_block
   `;
 }
