1de5017a7370944245cbbc65d46f472d72b2067d
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-02-25 19:02:46 +0100

    fix: return grouped analysis in shape for site

--- src/grouped_analysis_1.ts
+++ src/grouped_analysis_1.ts
@@ -21,7 +21,7 @@ export type GroupedAnalysis1 = {
   baseFeePerGas: number;
   burnRates: BurnRates.BurnRatesT;
   burnRecords: BurnRecordsCache.BurnRecordsCache["records"];
-  deflationaryStreak: DeflationaryStreak.DeflationaryStreakState;
+  deflationaryStreak: DeflationaryStreak.DeflationaryStreakForSite;
   ethPrice: EthPrices.EthStats | null;
   feeBurns: FeeBurn.FeesBurnedT;
   latestBlockFees: LatestBlockFees.LatestBlockFees;
@@ -109,7 +109,7 @@ export const updateAnalysis = (block: Blocks.BlockDb) =>
       "deflationaryStreak",
       Performance.measureTaskPerf(
         "get deflationary streak",
-        DeflationaryStreak.getStreakState(),
+        DeflationaryStreak.getStreakStateForSite(),
       ),
     ),
     T.map(
