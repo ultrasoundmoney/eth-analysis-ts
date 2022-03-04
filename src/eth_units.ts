037a157b338965eeb2ce03488fd3470a0aabd953
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-04 14:30:06 +0100

    refactor: remove misleading conversion

--- src/eth_units.ts
+++ src/eth_units.ts
@@ -2,7 +2,5 @@ export type Wei = number;
 export type WeiBI = bigint;
 
 export type Eth = number;
-export type EthBI = bigint;
 
-export const ethFromWeiBI = (wei: bigint): bigint => wei / 10n ** 18n;
 export const ethFromWei = (wei: number): number => wei / 1e18;
