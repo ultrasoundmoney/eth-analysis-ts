bd0615c7f2bca20263b3b105595b5d60718eb09a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-08 22:21:30 +0100

    fix: broken tests

--- src/integration-test/blocks.test.ts
+++ src/integration-test/blocks.test.ts
@@ -1,47 +1,28 @@
 import { test } from "uvu";
 import * as assert from "uvu/assert";
 import * as Blocks from "../blocks/blocks.js";
-import { BlockDb } from "../blocks/blocks.js";
 import { runMigrations, sql } from "../db.js";
-import * as SamplesBlocks from "../samples/blocks.js";
-
-const insertTestBlock = async (block: BlockDb) => {
-  const insertable = {
-    base_fee_per_gas: block.baseFeePerGas,
-    base_fee_sum: block.baseFeeSum,
-    gas_used: block.gasUsed,
-    eth_price: 1000,
-    hash: block.hash,
-    mined_at: block.minedAt,
-    number: block.number,
-  };
-
-  await sql`
-    INSERT INTO blocks
-      ${sql([insertable])}
-  `;
-};
-
-const removeTestBlock = async (blockNumber: number) => sql`
-  DELETE FROM blocks
-  WHERE number = ${blockNumber}
-`;
+import * as MockDb from "./mock_db.js";
 
 test.before(async () => {
   await runMigrations();
 });
 
-test("return the last stored block", async () => {
-  const block = await SamplesBlocks.getSingleBlock();
-  await insertTestBlock(block);
-  const storedBlock = await Blocks.getLastStoredBlock()();
-  assert.is(storedBlock.hash, block.hash);
-
-  await removeTestBlock(block.number);
+test.after.each(async () => {
+  MockDb.resetTables();
 });
 
 test.after(async () => {
   await sql.end();
+});
+
+test("return the last stored block", async () => {
+  await MockDb.seedBlocks("m5")();
+  const storedBlock = await Blocks.getLastStoredBlock()();
+  assert.is(
+    storedBlock.hash,
+    "0xd896114fc465d6217b94e9198de99502a990b3482756797a90ae3e2ee6dc1168",
+  );
 });
 
 test.run();
