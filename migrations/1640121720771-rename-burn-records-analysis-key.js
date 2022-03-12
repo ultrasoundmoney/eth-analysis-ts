632ffb4be238c8fe770a02c4f9a63d25356252d7
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-12 01:54:00 +0100

    fix: migration do nothing on conflict

--- migrations/1640121720771-rename-burn-records-analysis-key.js
+++ migrations/1640121720771-rename-burn-records-analysis-key.js
@@ -3,6 +3,7 @@ export async function up(client) {
     INSERT INTO analysis_state (key, last)
     SELECT 'burn-records', last FROM analysis_state
     WHERE key = 'burn_records'
+    ON CONFLICT DO NOTHING
   `;
 
   await client`
@@ -16,6 +17,7 @@ export async function down(client) {
     INSERT INTO analysis_state (key, last)
     SELECT 'burn_records', last FROM analysis_state
     WHERE key = 'burn-records'
+    ON CONFLICT DO NOTHING
   `;
 
   await client`
