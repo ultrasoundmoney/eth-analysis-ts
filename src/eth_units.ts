4af1670735276d6de060678fb1bf7dcf62de4f66
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-17 18:25:25 +0100

    feat: add wei from eth conversion

--- src/eth_units.ts
+++ src/eth_units.ts
@@ -4,3 +4,5 @@ export type WeiBI = bigint;
 export type Eth = number;
 
 export const ethFromWei = (wei: number): number => wei / 1e18;
+
+export const weiFromEth = (eth: number): number => eth * 1e18;
