b7d8571587d25dadf50e4c4f5247c4146aa37f62
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-20 16:07:55 +0000

    feat: integrate new fam details endpoint
    Has more details.
    More flexible.
    Updates one current use.

--- src/fam_service.ts
+++ src/fam_service.ts
@@ -1,44 +1,54 @@
-import fetch from "node-fetch";
+import * as Retry from "retry-ts";
 import * as Config from "./config.js";
-import { pipe, T } from "./fp.js";
-import * as Log from "./log.js";
+import * as FetchAlt from "./fetch_alt.js";
+import { NEA, pipe } from "./fp.js";
 
 export type FamDetails = {
-  bio: string | null;
-  famFollowerCount: number;
+  famFollowerCount: number | null;
   followersCount: number;
   handle: string;
+  isInFam: boolean | null;
   name: string;
+  twitterDescription: string;
+  twitterId: string;
 };
 
-export const getDetails = (handles: string[]): T.Task<FamDetails[]> => {
-  if (handles.length === 0) {
-    return T.of([]);
-  }
+const detailsByIdsUrl = `${Config.getFamServiceUrl()}/fam/leaderboards-details/ids`;
 
-  return pipe(
-    () =>
-      fetch(`${Config.getFamServiceUrl()}/fam/details`, {
-        method: "POST",
+export const getDetailsByIds = (twitterIds: NEA.NonEmptyArray<string>) =>
+  pipe(
+    FetchAlt.fetchWithRetryJson<FamDetails[]>(
+      detailsByIdsUrl,
+      {
+        body: JSON.stringify({ twitterIds }),
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ handles }),
-      }),
-    T.chain((res) => {
-      if (res.status === 500 || res.status === 502) {
-        // This happens sometimes, no need to crash but should figure out the issue on the fam service side.
-        Log.error(
-          "fetch fam details 500 or 502 response, returning empty list",
-        );
-        return T.of([]);
-      }
-
-      if (res.status !== 200) {
-        throw new Error(
-          `bad response fetching fam details, status: ${res.status}`,
-        );
-      }
-
-      return () => res.json() as Promise<FamDetails[]>;
-    }),
+        method: "POST",
+      },
+      {
+        retryPolicy: Retry.Monoid.concat(
+          Retry.exponentialBackoff(200),
+          Retry.limitRetries(2),
+        ),
+      },
+    ),
   );
-};
+
+const detailsByHandlesUrl = `${Config.getFamServiceUrl()}/fam/leaderboards-details/handles`;
+
+export const getDetailsByHandles = (handles: NEA.NonEmptyArray<string>) =>
+  pipe(
+    FetchAlt.fetchWithRetryJson<FamDetails[]>(
+      detailsByHandlesUrl,
+      {
+        body: JSON.stringify({ handles }),
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+      },
+      {
+        retryPolicy: Retry.Monoid.concat(
+          Retry.exponentialBackoff(200),
+          Retry.limitRetries(2),
+        ),
+      },
+    ),
+  );
