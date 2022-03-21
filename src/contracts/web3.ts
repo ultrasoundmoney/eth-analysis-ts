319751a4d7af7e5542b860ad17b8f952e246417f
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-21 12:36:16 +0000

    feat: get total supply for (proxy) contracts

--- src/contracts/web3.ts
+++ src/contracts/web3.ts
@@ -2,7 +2,7 @@ import QuickLRU from "quick-lru";
 import { Contract } from "web3-eth-contract";
 import * as Etherscan from "../etherscan.js";
 import * as EthNode from "../eth_node.js";
-import { B, O, pipe, TE, TEAlt } from "../fp.js";
+import { B, flow, O, pipe, TE, TEAlt } from "../fp.js";
 import * as Log from "../log.js";
 
 // NOTE: We already cache ABIs and creating contracts is cheap, but it turns out web3js leaks memory when creating new contracts. There's a _years_ old issue describing the problem here: https://github.com/ChainSafe/web3.js/issues/3042 . We'd like to switch to ethers-js for this and various other reasons. Until then, we cache contracts to alleviate the problem a little.
@@ -87,15 +87,81 @@ export const getSupportedInterface = (
     ),
   );
 
-export const getTotalSupply = (contract: Contract) =>
+type Erc20Contract = Omit<Contract, "methods"> & {
+  methods: { totalSupply: () => { call: () => Promise<number> } };
+};
+
+type ProxyContract = Omit<Contract, "methods"> & {
+  methods: { implementation: () => { call: () => Promise<string> } };
+};
+
+const getIsProxyContract = (contract: Contract) =>
+  contract.methods.implementation !== undefined;
+
+const getIsErc20Contract = (contract: Contract) =>
+  contract.methods.totalSupply !== undefined;
+
+const getErc20TotalSupply = TE.tryCatchK(
+  (erc20Contract: Erc20Contract) => erc20Contract.methods.totalSupply().call(),
+  TEAlt.decodeUnknownError,
+);
+
+export class UnsupportedContractError extends Error {}
+export class ExecutionRevertedError extends Error {}
+
+const decodeContractCallError = (e: unknown) => {
+  if (e instanceof Error) {
+    if (e.message.includes("execution reverted")) {
+      return new ExecutionRevertedError();
+    }
+
+    return e;
+  }
+
+  return new Error(String(e));
+};
+
+const getErc20ProxyTotalSupply = flow(
+  TE.tryCatchK(
+    (proxyContract: ProxyContract) =>
+      proxyContract.methods.implementation().call(),
+    decodeContractCallError,
+  ),
+  TE.chainW((implementationAddress) => getContract(implementationAddress)),
+  TE.chainW((implementationContract) =>
+    pipe(
+      getIsErc20Contract(implementationContract),
+      B.match(
+        () =>
+          TE.left(
+            new UnsupportedContractError(
+              `expected ERC20 proxy implementation contract ${implementationContract.options.address} to have an totalSupply method but it didn't`,
+            ),
+          ),
+        () => getErc20TotalSupply(implementationContract),
+      ),
+    ),
+  ),
+  TE.mapLeft(
+    (e): UnsupportedContractError | Etherscan.FetchAbiError | Error => {
+      // We expect for this to happen on some proxy contracts that make their implementation method adminOnly, consider them simply unsupported.
+      if (e instanceof ExecutionRevertedError) {
+        return new UnsupportedContractError(e.message);
+      }
+
+      return e;
+    },
+  ),
+);
+
+export const getTotalSupply = (address: string) =>
   pipe(
-    contract.methods["totalSupply"] === undefined,
-    B.match(
-      () =>
-        TE.tryCatch(
-          () => contract.methods.totalSupply().call() as Promise<number>,
-          TEAlt.errorFromUnknown,
-        ),
-      () => TE.left(new UnsupportedMethodError()),
+    getContract(address),
+    TE.chainW((contract) =>
+      getIsErc20Contract(contract)
+        ? getErc20TotalSupply(contract)
+        : getIsProxyContract(contract)
+        ? getErc20ProxyTotalSupply(contract)
+        : TE.left(new UnsupportedContractError()),
     ),
   );
