822c5b50641638397682a45c5fb6cb41ff5a5803
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-21 16:59:28 +0000

    feat: add module to mark timestamps

---
+++ src/time.ts
@@ -0,0 +1,11 @@
+/**
+ * A JavaScript timestamp denoting milliseconds since unix epoch.
+ *
+ */
+export type JsTimestamp = number;
+
+/**
+ * A unix timestamp denoting seconds since unix epoch.
+ *
+ */
+export type UnixTimestamp = number;
