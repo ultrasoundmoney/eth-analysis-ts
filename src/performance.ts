80d41d89a6a84eef03a032b8779da7dff86918b9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-31 15:15:02 +0100

    feat: allow setting level on performance log

--- src/performance.ts
+++ src/performance.ts
@@ -2,10 +2,14 @@ import { performance } from "perf_hooks";
 import { pipe, T } from "./fp.js";
 import * as Log from "./log.js";
 
-export const logPerf = (name: string, t0: number): void => {
+export const logPerf = (
+  name: string,
+  t0: number,
+  level: Log.Level = "DEBUG",
+): void => {
   const t1 = performance.now();
   const took = ((t1 - t0) / 1000).toFixed(2);
-  Log.debug(`${name} took ${took}s`);
+  Log.log(level, `${name} took ${took}s`);
 };
 
 export const logPerfT = (name: string, t0: number) => () => () => {
