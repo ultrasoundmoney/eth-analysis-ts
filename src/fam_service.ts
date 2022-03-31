032de52264e696002a20294a5f4610072bdef333
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-03-31 22:38:51 +0100

    feat: remove unneeded wrapping fn

--- src/fam_service.ts
+++ src/fam_service.ts
@@ -1,7 +1,7 @@
 import * as Retry from "retry-ts";
 import * as Config from "./config.js";
 import * as FetchAlt from "./fetch_alt.js";
-import { NEA, pipe } from "./fp.js";
+import { NEA } from "./fp.js";
 
 export type LinkableUrl = {
   display_url: string;
@@ -49,39 +49,35 @@ export type TwitterDetails = {
 const detailsByIdsUrl = `${Config.getFamServiceUrl()}/fam/leaderboards-details/ids`;
 
 export const getDetailsByIds = (twitterIds: NEA.NonEmptyArray<string>) =>
-  pipe(
-    FetchAlt.fetchWithRetryJson<TwitterDetails[]>(
-      detailsByIdsUrl,
-      {
-        body: JSON.stringify({ twitterIds }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      },
-      {
-        retryPolicy: Retry.Monoid.concat(
-          Retry.exponentialBackoff(200),
-          Retry.limitRetries(2),
-        ),
-      },
-    ),
+  FetchAlt.fetchWithRetryJson<TwitterDetails[]>(
+    detailsByIdsUrl,
+    {
+      body: JSON.stringify({ twitterIds }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    },
+    {
+      retryPolicy: Retry.Monoid.concat(
+        Retry.exponentialBackoff(200),
+        Retry.limitRetries(2),
+      ),
+    },
   );
 
 const detailsByHandlesUrl = `${Config.getFamServiceUrl()}/fam/leaderboards-details/handles`;
 
 export const getDetailsByHandles = (handles: NEA.NonEmptyArray<string>) =>
-  pipe(
-    FetchAlt.fetchWithRetryJson<TwitterDetails[]>(
-      detailsByHandlesUrl,
-      {
-        body: JSON.stringify({ handles }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      },
-      {
-        retryPolicy: Retry.Monoid.concat(
-          Retry.exponentialBackoff(200),
-          Retry.limitRetries(2),
-        ),
-      },
-    ),
+  FetchAlt.fetchWithRetryJson<TwitterDetails[]>(
+    detailsByHandlesUrl,
+    {
+      body: JSON.stringify({ handles }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    },
+    {
+      retryPolicy: Retry.Monoid.concat(
+        Retry.exponentialBackoff(200),
+        Retry.limitRetries(2),
+      ),
+    },
   );
