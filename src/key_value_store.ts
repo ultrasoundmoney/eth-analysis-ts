c81731d97410718f5f875d1f572cfc65b374b0c5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-01 13:56:52 +0100

    feat: add a key value store module

---
+++ src/key_value_store.ts
@@ -0,0 +1,28 @@
+import * as Db from "./db.js";
+import { O, pipe, T, TOAlt } from "./fp.js";
+
+export const getValue = <A>(key: string) =>
+  pipe(
+    Db.sqlT<{ value: A }[]>`
+      SELECT value FROM key_value_store
+      WHERE key = ${key}
+    `,
+    T.map(O.fromNullableK((rows) => rows[0]?.value)),
+  );
+
+export const getValueUnsafe = <A>(key: string) =>
+  pipe(
+    getValue<A>(key),
+    TOAlt.getOrThrow(`expected a value to exist for key ${key}`),
+  );
+
+export const setValue = (key: string, value: unknown) =>
+  Db.sqlTVoid`
+    INSERT INTO key_value_store
+      ${Db.values({
+        key,
+        value: JSON.stringify(value),
+      })}
+    ON CONFLICT (key) DO UPDATE SET
+      value = excluded.value
+  `;
