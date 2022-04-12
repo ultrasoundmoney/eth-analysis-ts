a8de3d1e0877bc84e810715b702112ae89f1ccd5
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-12 15:04:09 +0200

    feat: improve default request handling

--- src/fetch_alt.ts
+++ src/fetch_alt.ts
@@ -1,8 +1,7 @@
 import nodeFetch, * as NodeFetch from "node-fetch";
 import * as Retry from "retry-ts";
 import { retrying } from "retry-ts/lib/Task.js";
-import { match, P } from "ts-pattern";
-import { E, flow, IO, pipe, TE } from "./fp.js";
+import { E, IO, O, pipe, T, TE } from "./fp.js";
 import * as Log from "./log.js";
 
 export class DecodeJsonError extends Error {}
@@ -19,6 +18,16 @@ export class BadResponseError extends Error {
 
 const defaultAcceptStatuses = [200, 201, 202, 204, 206];
 
+const parseMessageFromUnknown = (u: unknown) =>
+  pipe(
+    typeof u === "string" ? O.some(u) : O.none,
+    O.alt(() =>
+      typeof (u as { message?: string })?.message === "string"
+        ? O.some((u as { message: string }).message)
+        : O.none,
+    ),
+  );
+
 export const fetch = (
   url: NodeFetch.RequestInfo,
   init?: NodeFetch.RequestInit,
@@ -27,22 +36,19 @@ export const fetch = (
   pipe(
     TE.tryCatch(
       () => nodeFetch(url, init),
-      (e): FetchError | Error =>
-        match(e)
-          .with(P.instanceOf(FetchError), (e) => e)
-          .with(P.instanceOf(Error), (e) => e)
-          .otherwise((u) => new Error(String(u))),
+      (u) =>
+        u instanceof FetchError
+          ? u
+          : u instanceof Error
+          ? u
+          : new Error(String(u)),
     ),
     TE.chainW((res) =>
-      match(res.status)
-        .when(
-          (status) => acceptStatuses.includes(status),
-          () => TE.right(res),
-        )
-        .otherwise(
-          flow(
+      acceptStatuses.includes(res.status)
+        ? TE.right(res)
+        : pipe(
             // We try to decode a json error body to put on the error, but do not expect it to work.
-            TE.tryCatchK(
+            TE.tryCatch(
               () => res.json(),
               // Failed to decode json error body. This is within expectations
               () =>
@@ -53,31 +59,34 @@ export const fetch = (
             ),
             // We managed to decode a json body despite the bad response.
             TE.chain((body) =>
-              TE.left(
-                new BadResponseError(
-                  `failed to fetch ${url}, got status ${res.status}`,
-                  res.status,
-                  body,
+              pipe(
+                parseMessageFromUnknown(body),
+                O.match(
+                  () => `failed to fetch ${url}, got status ${res.status}`,
+                  (message) =>
+                    `failed to fetch ${url}, status: ${res.status}, message: ${message}`,
                 ),
+                (message) =>
+                  TE.left(new BadResponseError(message, res.status, body)),
               ),
             ),
           ),
-        ),
     ),
   );
 
-export const decodeJsonResponse = TE.tryCatchK(
+const decodeJsonResponse = TE.tryCatchK(
   (res: NodeFetch.Response) => res.json(),
   (e) =>
-    match(e)
-      .with(P.instanceOf(Error), (e) => new DecodeJsonError(e.message))
-      .otherwise((u) => new DecodeJsonError(String(u))),
+    e instanceof Error
+      ? new DecodeJsonError(e.message)
+      : new DecodeJsonError(String(e)),
 );
+
 export const fetchJson = (
   url: NodeFetch.RequestInfo,
   init?: NodeFetch.RequestInit,
   acceptStatuses: number[] = defaultAcceptStatuses,
-) => pipe(fetch(url, init, acceptStatuses), TE.chain(decodeJsonResponse));
+) => pipe(fetch(url, init, acceptStatuses), TE.chainW(decodeJsonResponse));
 
 const defaultRetryPolicy = Retry.Monoid.concat(
   Retry.exponentialBackoff(2000),
@@ -105,12 +114,18 @@ export const fetchWithRetry = (
     (status) =>
       pipe(
         fetch(url, init, acceptStatuses),
-        TE.chainFirstIOK((res) =>
-          status.iterNumber === 0
-            ? IO.of(undefined)
-            : Log.debugIO(
-                `retrying request ${url}, last status was: ${res.status}, attempt: ${status.iterNumber}, wait sum: ${status.cumulativeDelay}ms`,
-              ),
+        T.chainFirstIOK(
+          E.match(
+            (e) =>
+              e instanceof BadResponseError
+                ? Log.debugIO(
+                    `retrying request ${url}, last status was: ${e.status}, attempt: ${status.iterNumber}, wait sum: ${status.cumulativeDelay}ms`,
+                  )
+                : Log.debugIO(
+                    `retrying request ${url}, attempt: ${status.iterNumber}, wait sum: ${status.cumulativeDelay}ms`,
+                  ),
+            () => IO.of(undefined),
+          ),
         ),
       ),
     E.match(
