abc586b28686cfe7e742519e6667f90878a64950
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-13 20:50:08 +0200

    feat: add standard queues module

---
+++ src/queues.ts
@@ -0,0 +1,25 @@
+import PQueue from "p-queue";
+import { E, pipe, TE } from "./fp.js";
+
+export class TimeoutError extends Error {}
+
+const getIsPQueueTimeoutError = (u: unknown): boolean =>
+  typeof (u as { name: string })?.name === "string" &&
+  (u as { name: string }).name === "TimeoutError";
+
+export const queueOnQueue =
+  <E, A>(queue: PQueue) =>
+  (task: TE.TaskEither<E, A>) =>
+    pipe(
+      TE.tryCatch(
+        () => queue.add<E.Either<E, A> | undefined>(task as never),
+        (error) =>
+          getIsPQueueTimeoutError(error) ? new TimeoutError() : (error as E),
+      ),
+      // tryCatch expects a simple () => Promise<A> but we're passing a () => Promise<Either<E, A>>, tryCatch then wraps the return value in a TaskEither for us creating a TaskEither<E1, Either<E2, A>>, we want TaskEither<E1 | E2, A>, the below fn achieves just that.
+      TE.chainEitherKW((either) =>
+        either === undefined
+          ? E.left<E | TimeoutError>(new TimeoutError())
+          : either,
+      ),
+    );
