b75aaaa0ccce59b1e0563933a012d78a1bfacc62
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-25 17:41:37 +0200

    fix: import and needless wrapping

--- src/debug/debug_contract_metadata.ts
+++ src/debug/debug_contract_metadata.ts
@@ -1,4 +1,4 @@
-import * as ContractsMetadata from "../contracts/crawl_metadata.js";
+import { addMetadataForAddresses } from "../contracts/metadata/metadata.js";
 import { sql } from "../db.js";
 import * as EthNode from "../eth_node.js";
 import * as Log from "../log.js";
@@ -7,7 +7,7 @@ const main = async () => {
   try {
     Log.info("starting add-contract-metadata");
 
-    await ContractsMetadata.addMetadataForLeaderboards(
+    await addMetadataForAddresses(
       ["0x59c0e4b889f4c036dd0d6d759c7b37cf91f3ec01"],
       new Set(),
     )();
