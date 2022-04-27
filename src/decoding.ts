3247761d8d9ad640c839ac7559e8dd60c48d2a44
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-27 12:23:44 +0200

    feat: add decode with error

--- src/decoding.ts
+++ src/decoding.ts
@@ -1,6 +1,16 @@
-import { D, pipe } from "./fp.js";
+import { D, E, pipe } from "./fp.js";
 
 export const decodeEmptyString = pipe(
   D.string,
   D.parse((s) => (s === "" ? D.success(null) : D.success(s))),
 );
+
+export class DecodeError extends Error {}
+
+export const decodeWithError =
+  <A>(decoder: D.Decoder<unknown, A>) =>
+  (u: unknown) =>
+    pipe(
+      decoder.decode(u),
+      E.mapLeft((e) => new DecodeError(`failed to decode\n${D.draw(e)}`)),
+    );
