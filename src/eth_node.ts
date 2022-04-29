bc702fb932544d78e1d993464e4d761c1d625739
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-04-29 12:56:47 +0200

    feat: simplify failed connection handling

--- src/eth_node.ts
+++ src/eth_node.ts
@@ -1,78 +1,16 @@
-import PQueue from "p-queue";
 import * as DateFns from "date-fns";
+import PQueue from "p-queue";
 import Web3 from "web3";
 import web3Core from "web3-core";
 import { Contract } from "web3-eth-contract";
 import { AbiItem } from "web3-utils";
 import WebSocket from "ws";
 import * as Config from "./config.js";
-import * as Duration from "./duration.js";
-import { pipe, T } from "./fp.js";
 import * as Hexadecimal from "./hexadecimal.js";
 import * as Log from "./log.js";
 
 // TODO: restart whole service when websocket fails.
 // Try own node three times, then switch to fallback node.
-
-type LogWeb3 = web3Core.Log;
-const WebsocketProvider = web3Core.WebsocketProvider;
-
-let managedWeb3Obj: Web3 | undefined = undefined;
-
-let managedGethWs: WebSocket | undefined = undefined;
-
-const messageListners = new Map();
-
-let wsAttempt = 0;
-
-const connect = async (): Promise<WebSocket> => {
-  if (Config.getUseNodeFallback()) {
-    Log.warn("using node fallback");
-  }
-
-  // Try our own node three times then try our third party fallback
-  const ws =
-    wsAttempt < 3 && !Config.getUseNodeFallback()
-      ? new WebSocket(Config.getGethUrl())
-      : new WebSocket(Config.getGethFallbackUrl());
-
-  if (wsAttempt < 3) {
-    wsAttempt = wsAttempt + 1;
-  } else {
-    Log.error("failed to connect to geth node, using fallback");
-    wsAttempt = 0;
-  }
-
-  ws.on("message", (event) => {
-    const message: {
-      id: number;
-      result: unknown;
-      error: { code: number; message: string };
-    } = JSON.parse(event.toString());
-
-    const cb = messageListners.get(message.id);
-
-    if (cb !== undefined) {
-      if ("error" in message) {
-        cb(message.error);
-      } else {
-        cb(null, message.result);
-      }
-    }
-  });
-
-  ws.on("close", () => {
-    Log.info("geth node websocket closed, immediately reconnecting");
-    getGethWs();
-  });
-
-  return new Promise((resolve) => {
-    ws.on("open", () => {
-      Log.debug("connected to eth node");
-      resolve(ws);
-    });
-  });
-};
 
 type MessageErr = { code: number; message: string };
 
@@ -84,7 +22,10 @@ const getNewMessageId = (): number => {
     nextId = 0;
   }
 
-  // If more than 1024 messages are still waiting for a response, this loop starts eating CPU until an id frees up. We assume this is never the case, if it does start happening, increase the id pool.
+  if (inUseIds.size === 1024) {
+    Log.alert("message id pool hit limit, increase pool size or reduce wait!");
+  }
+
   while (inUseIds.has(nextId)) {
     nextId = nextId + 1;
   }
@@ -96,8 +37,8 @@ const getNewMessageId = (): number => {
 const registerMessageListener = <A>(): [number, Promise<A>] => {
   const id = getNewMessageId();
   const messageP: Promise<A> = new Promise((resolve, reject) => {
-    messageListners.set(id, (err: MessageErr, data: A) => {
-      messageListners.delete(id);
+    messageCallbackMap.set(id, (err: MessageErr, data: A) => {
+      messageCallbackMap.delete(id);
       inUseIds.delete(id);
       if (err !== null) {
         reject(err);
@@ -110,63 +51,60 @@ const registerMessageListener = <A>(): [number, Promise<A>] => {
   return [id, messageP];
 };
 
-let web3Attempt = 0;
+let managedWeb3Obj: Web3 | undefined = undefined;
 
-export const getWeb3 = (): Web3 => {
-  if (managedWeb3Obj !== undefined) {
-    return managedWeb3Obj;
-  }
+let managedGethWs: WebSocket | undefined = undefined;
 
-  const providerOptions = {
-    reconnect: {
-      auto: true,
-      delay: Duration.millisFromSeconds(5),
-      maxAttempts: 5,
-    },
-  };
-  // Try our own node three times then try our third party fallback.
-  const provider =
-    web3Attempt < 3 && !Config.getUseNodeFallback()
-      ? new Web3.providers.WebsocketProvider(
-          Config.getGethUrl(),
-          providerOptions,
-        )
-      : new Web3.providers.WebsocketProvider(
-          Config.getGethFallbackUrl(),
-          providerOptions,
-        );
+const messageCallbackMap = new Map();
 
-  if (web3Attempt < 3) {
-    web3Attempt = wsAttempt + 1;
-  } else {
-    Log.error("failed to connect to geth node, using fallback");
-    web3Attempt = 0;
-  }
+const makeWs = (url: string) => {
+  const ws = new WebSocket(url);
 
-  managedWeb3Obj = new Web3(provider);
-  return managedWeb3Obj;
+  return new Promise<WebSocket>((resolve, reject) => {
+    ws.on("reject", (error) => {
+      reject(error);
+    });
+
+    ws.on("open", () => {
+      Log.debug("connected to eth node");
+      resolve(ws);
+    });
+  });
 };
 
-const connectionQueue = new PQueue({ concurrency: 1 });
+const connect = async (): Promise<WebSocket> => {
+  const ws = await makeWs(
+    Config.getUseNodeFallback()
+      ? Config.getGethFallbackUrl()
+      : Config.getGethUrl(),
+  );
 
-const getGethWs = () => connectionQueue.add(getOpenSocketOrReconnect);
+  ws.on("message", (event) => {
+    const message = JSON.parse(event.toString()) as {
+      id: number;
+      result: unknown;
+      error: { code: number; message: string };
+    };
 
-export const closeConnections = async () => {
-  if (connectionQueue.size !== 0) {
-    await connectionQueue.onEmpty();
-  }
+    const cb = messageCallbackMap.get(message.id);
 
-  if (managedGethWs !== undefined) {
-    managedGethWs.close();
-  }
+    if (cb === undefined) {
+      Log.error("failed to find callback for message in callback map");
+      return;
+    }
 
-  if (
-    managedWeb3Obj !== undefined &&
-    managedWeb3Obj.currentProvider !== null &&
-    managedWeb3Obj.currentProvider instanceof WebsocketProvider
-  ) {
-    managedWeb3Obj.currentProvider.disconnect(0, "exiting");
-  }
+    if ("error" in message) {
+      cb(message.error);
+    } else {
+      cb(null, message.result);
+    }
+  });
+
+  ws.on("close", () => {
+    Log.info("geth node websocket closed");
+  });
+
+  return ws;
 };
 
 const getOpenSocketOrReconnect = async (): Promise<WebSocket> => {
@@ -191,6 +129,75 @@ const getOpenSocketOrReconnect = async (): Promise<WebSocket> => {
   const ws = await connect();
   managedGethWs = ws;
   return ws;
+};
+
+const gethWsSeqQueue = new PQueue({ concurrency: 1 });
+
+const getGethWs = () => gethWsSeqQueue.add(getOpenSocketOrReconnect);
+
+const connectWeb3 = (url: string) =>
+  new Promise<web3Core.WebsocketProvider>((resolve, reject) => {
+    const provider = new Web3.providers.WebsocketProvider(url, {
+      timeout: 8000,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const onError: any = (error: unknown) => {
+      reject(error);
+    };
+
+    provider.on("error", onError);
+
+    provider.on("connect", () => {
+      resolve(provider);
+    });
+
+    provider.connect();
+  });
+
+export const getExistingWeb3OrReconnect = async (): Promise<Web3> => {
+  if (managedWeb3Obj !== undefined) {
+    return Promise.resolve(managedWeb3Obj);
+  }
+
+  if (Config.getUseNodeFallback()) {
+    const provider = await connectWeb3(Config.getGethFallbackUrl());
+    return new Web3(provider);
+  }
+
+  try {
+    const provider = await connectWeb3(Config.getGethUrl());
+    const web3 = new Web3(provider);
+    managedWeb3Obj = web3;
+    return managedWeb3Obj;
+  } catch (error) {
+    const provider = await connectWeb3(Config.getGethFallbackUrl());
+    const web3 = new Web3(provider);
+    managedWeb3Obj = web3;
+    return managedWeb3Obj;
+  }
+};
+
+const web3SeqQueue = new PQueue({ concurrency: 1 });
+
+export const getWeb3 = () => web3SeqQueue.add(getExistingWeb3OrReconnect);
+
+export const closeConnections = async () => {
+  if (gethWsSeqQueue.size !== 0) {
+    await gethWsSeqQueue.onEmpty();
+  }
+
+  if (managedGethWs !== undefined) {
+    managedGethWs.close();
+  }
+
+  if (
+    managedWeb3Obj !== undefined &&
+    managedWeb3Obj.currentProvider !== null &&
+    managedWeb3Obj.currentProvider instanceof web3Core.WebsocketProvider
+  ) {
+    managedWeb3Obj.currentProvider.disconnect(0, "exiting");
+  }
 };
 
 const send = async (message: Record<string, unknown>) => {
@@ -275,7 +282,7 @@ export type RawTxr = {
   effectiveGasPrice: string;
   from: string;
   gasUsed: string;
-  logs: LogWeb3[];
+  logs: web3Core.Log[];
   logsBloom: string;
   status: string;
   to: string;
@@ -347,28 +354,16 @@ export const subscribeNewHeads = (
     const rawHead: RawHead = JSON.parse(data.toString()).params.result;
     const head = translateHead(rawHead);
 
-    // const receivedAt = new Date().toISOString();
-    // const minedAt = new Date(Number(head.timestamp) * 1000).toISOString();
-    // Log.debug("new head", {
-    //   number: head.number,
-    //   hash: head.hash,
-    //   parentHash: head.parentHash,
-    //   receivedAt,
-    //   minedAt,
-    // });
-
     Log.debug(
       `new head, number: ${head.number}, hash: ${head.hash}, parent: ${head.parentHash}`,
     );
 
-    const formatter = new Intl.NumberFormat("en", { maximumFractionDigits: 2 });
+    const stalenessOnReceive = DateFns.differenceInSeconds(
+      new Date(),
+      DateFns.fromUnixTime(Number(rawHead.timestamp)),
+    );
     Log.debug(
-      `miner to api block staleness ${formatter.format(
-        DateFns.differenceInSeconds(
-          new Date(),
-          DateFns.fromUnixTime(Number(rawHead.timestamp)),
-        ),
-      )}s, timestamp: ${new Date().toISOString()}`,
+      `miner to api block staleness ${stalenessOnReceive}s, timestamp: ${new Date().toISOString()}`,
     );
 
     handleNewHead(head);
@@ -437,15 +432,21 @@ export const getLatestBlockNumber = async (): Promise<number> => {
   return Hexadecimal.numberFromHex(rawBlockNumber);
 };
 
-export const makeContract = (address: string, abi: AbiItem[]): Contract => {
-  const Contract = getWeb3().eth.Contract;
+export const makeContract = async (address: string, abi: AbiItem[]) => {
+  const web3 = await getWeb3();
+  const Contract = web3.eth.Contract;
   const contract = new Contract(abi, address);
   // NOTE: possible workaround as web3 leaks memory.
   // See: https://github.com/ChainSafe/web3.js/issues/3042#issuecomment-663622882
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (contract as any).setProvider(getWeb3().currentProvider);
+  type ContractWithProvider = Contract & {
+    setProvider: (provider: web3Core.provider) => void;
+  };
+  (contract as ContractWithProvider).setProvider(web3.currentProvider);
   return contract;
 };
 
-export const getBalance = (address: string) =>
-  pipe(() => getWeb3().eth.getBalance(address), T.map(BigInt));
+export const getBalance = async (address: string) => {
+  const web3 = await getWeb3();
+  const balanceStr = await web3.eth.getBalance(address);
+  return BigInt(balanceStr);
+};
