57e69f0657956383aa95880ceaaa776e65ad7b63
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-05 09:36:24 +0200

    fix: canary debounce should lead
    Fixes a log message too.

--- src/canary.ts
+++ src/canary.ts
@@ -11,31 +11,35 @@ const cage: Record<CanaryType, NodeJS.Timeout | undefined> = {
   leaderboard: undefined,
 };
 
-const durationMilis = Duration.millisFromMinutes(5);
+const durationMilis = Duration.millisFromMinutes(6);
 
-const fireAlarm = _.debounce(async () => {
-  Log.alert(`canary dead, no block for ${durationMilis / 1000}s`);
+const fireAlarm = _.debounce(
+  async () => {
+    Log.alert(`canary dead, no block for ${durationMilis / 1000}s`);
 
-  const res = await fetch("https://api.opsgenie.com/v2/alerts", {
-    method: "POST",
-    headers: {
-      Authorization: `GenieKey ${opsGenieApiKey}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: "no block for more than 3 minutes!",
-    }),
-  });
+    const res = await fetch("https://api.opsgenie.com/v2/alerts", {
+      method: "POST",
+      headers: {
+        Authorization: `GenieKey ${opsGenieApiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: `no block for more than ${durationMilis / 1000 / 60} minutes!`,
+      }),
+    });
 
-  if (!res.ok) {
-    const body = (await res.json()) as {
-      message: string;
-      took: number;
-      requestId: string;
-    };
-    throw new Error(`OpsGenie alert request failed! ${body.message}`);
-  }
-}, Duration.millisFromMinutes(4));
+    if (!res.ok) {
+      const body = (await res.json()) as {
+        message: string;
+        took: number;
+        requestId: string;
+      };
+      throw new Error(`OpsGenie alert request failed! ${body.message}`);
+    }
+  },
+  Duration.millisFromMinutes(4),
+  { leading: true },
+);
 
 export const releaseCanary = (): void => {
   cage.block = setTimeout(fireAlarm, durationMilis);
