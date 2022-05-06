256e07755dc0717e414adbe7da2f622f7c7d7a26
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-06 14:03:30 +0200

    feat add logging for validator sum storing

--- src/validator_balances.ts
+++ src/validator_balances.ts
@@ -2,25 +2,39 @@ import * as DateFns from "date-fns";
 import * as Db from "./db.js";
 import { ethFromGwei } from "./eth_units.js";
 import { A, pipe, T } from "./fp.js";
+import * as Log from "./log.js";
 
 export const genesisTimestamp = DateFns.fromUnixTime(1606824023);
 
 const getTimestampFromSlot = (slot: number) =>
   DateFns.addSeconds(genesisTimestamp, slot * 12);
 
+const getDayTimestampFromSlot = (slot: number) =>
+  pipe(slot, getTimestampFromSlot, DateFns.startOfDay);
+
 export const storeValidatorSumForDay = (
   slot: number,
   validatorBalanceSum: bigint,
 ) =>
   pipe(
-    Db.sqlTVoid`
-      INSERT INTO eth_in_validators
-        ${Db.values({
-          date_at: pipe(slot, getTimestampFromSlot, DateFns.startOfDay),
-          gwei: validatorBalanceSum,
-        })}
-      ON CONFLICT (date_at) DO NOTHING
-    `,
+    getDayTimestampFromSlot(slot),
+    T.of,
+    T.chainFirstIOK((dateAt) =>
+      Log.debugIO(
+        `storing validator sum for day, slot: ${slot}, date_at: ${dateAt.toISOString()}, gwei: ${validatorBalanceSum}`,
+      ),
+    ),
+    T.chain(
+      (dateAt) =>
+        Db.sqlTVoid`
+            INSERT INTO eth_in_validators
+              ${Db.values({
+                date_at: dateAt,
+                gwei: validatorBalanceSum,
+              })}
+            ON CONFLICT (date_at) DO NOTHING
+          `,
+    ),
   );
 
 export const getEthInValidatorsByDay = () =>
