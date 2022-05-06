8c2b5c9265e2e4a9d6928e24e90ce3134a3f5758
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-06 14:17:17 +0200

    feat: use timestamp instead of date

--- migrations/1640121720776-add-daily-validator-balances.js
+++ migrations/1640121720776-add-daily-validator-balances.js
@@ -1,9 +1,9 @@
 export async function up(client) {
   await client`
     CREATE TABLE eth_in_validators (
-      date_at date NOT NULL,
+      timestamp timestamptz NOT NULL,
       gwei numeric NOT NULL,
-      CONSTRAINT eth_in_validators_pk PRIMARY KEY ("date_at")
+      CONSTRAINT eth_in_validators_pk PRIMARY KEY ("timestamp")
     );
   `;
 }
