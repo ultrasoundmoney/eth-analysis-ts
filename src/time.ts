4cfb3898287603526d8051a1a5b01381492f981c
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-07 15:31:16 +0200

    feat: add beacon issuance to supply projection

--- src/time.ts
+++ src/time.ts
@@ -1,11 +1,9 @@
 /**
  * A JavaScript timestamp denoting milliseconds since unix epoch.
- *
  */
 export type JsTimestamp = number;
 
 /**
  * A unix timestamp denoting seconds since unix epoch.
- *
  */
 export type UnixTimestamp = number;
