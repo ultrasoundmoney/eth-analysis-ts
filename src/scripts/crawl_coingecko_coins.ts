83a5f60786621f9ea18c7d8a7aff8a6efdd97d85
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-05-20 13:10:21 +0200

    feat: add crawl coingecko coins script

---
+++ src/scripts/crawl_coingecko_coins.ts
@@ -0,0 +1,42 @@
+import * as Coingecko from "../coingecko.js";
+import * as MetadataCoingecko from "../contracts/metadata/coingecko.js";
+import { A, pipe, T, TE, TEAlt } from "../fp.js";
+import * as TotalValueSecured from "../total-value-secured/total_value_secured.js";
+import * as Log from "../log.js";
+import { addTwitterMetadataMaybe } from "../contracts/metadata/twitter.js";
+
+let seenLastCrawled = true;
+const lastCrawled = "";
+
+await pipe(
+  Coingecko.getCoinList(),
+  TE.map(A.filter(TotalValueSecured.getIsCoinOnEth)),
+  TE.map(
+    A.filter((coin) => {
+      if (seenLastCrawled) {
+        return true;
+      }
+
+      if (coin.id === lastCrawled) {
+        seenLastCrawled = true;
+        return false;
+      }
+
+      return false;
+    }),
+  ),
+  TEAlt.chainFirstLogDebug((list) => `${list.length} coins to crawl`),
+  TE.chain(
+    TE.traverseSeqArray((coin) =>
+      pipe(
+        MetadataCoingecko.checkForMetadata(coin.platforms.ethereum),
+        T.chain(() => addTwitterMetadataMaybe(coin.platforms.ethereum)),
+        TE.fromTask,
+      ),
+    ),
+  ),
+  TE.match(
+    (e) => Log.error("failed to crawl CoinGecko metadata", e),
+    () => Log.debug("succcessfully crawled CoinGecko metadata"),
+  ),
+)();
