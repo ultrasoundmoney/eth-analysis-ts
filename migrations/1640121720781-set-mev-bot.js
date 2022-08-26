5cbe9d6991abc4b5d0af267a4585dfd7b755a353
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-08-27 01:19:32 +0200

    style: format SQL

--- migrations/1640121720781-set-mev-bot.js
+++ migrations/1640121720781-set-mev-bot.js
@@ -1,9 +1,13 @@
 export async function up(client) {
   await client`
-		UPDATE contracts
-		SET category = 'mev', is_bot = TRUE
-		WHERE name ILIKE 'mev bot:%'
-	`;
+    UPDATE
+      contracts
+    SET
+      category = 'mev',
+      is_bot = TRUE
+    WHERE
+      name ILIKE 'mev bot:%'
+  `;
 }
 
 export async function down() {}
