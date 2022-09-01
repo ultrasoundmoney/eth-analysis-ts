9b43554841cbcf52f5d36c87497017ad0641ca0a
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-09-01 14:00:27 +0200

    feat: don't send block lag if nothing changed

--- src/block_lag.ts
+++ src/block_lag.ts
@@ -5,6 +5,7 @@ import * as ExecutionNode from "./execution_node.js";
 import { O, pipe, T, TO } from "./fp.js";
 import { getLatestGroupedAnalysisNumber } from "./grouped_analysis_1.js";
 import * as KeyValueStore from "./key_value_store.js";
+import * as Log from "./log.js";
 
 export const blockLagCacheKey = "block-lag";
 
@@ -13,7 +14,8 @@ const updateCurrentBlockLag = () =>
     T.Do,
     T.apS("latestBlockNumber", () => ExecutionNode.getLatestBlockNumber()),
     T.apS("latestGroupedAnalysisNumber", getLatestGroupedAnalysisNumber()),
-    T.map(({ latestBlockNumber, latestGroupedAnalysisNumber }) =>
+    T.apS("lastBlockLag", KeyValueStore.getValue<number>(blockLagCacheKey)),
+    T.map(({ lastBlockLag, latestBlockNumber, latestGroupedAnalysisNumber }) =>
       pipe(
         latestGroupedAnalysisNumber,
         O.map((latestGroupedAnalysisNumber) =>
@@ -21,12 +23,27 @@ const updateCurrentBlockLag = () =>
             ? 1
             : latestBlockNumber - latestGroupedAnalysisNumber,
         ),
+        O.chain((blockLag) =>
+          pipe(
+            lastBlockLag,
+            O.match(
+              // We never stored a block lag, broadcast the new one.
+              () => O.some(blockLag),
+              (lastBlockLag) =>
+                // If we have a block lag stored, only broadcast a new one if it changed.
+                blockLag === lastBlockLag ? O.none : O.some(blockLag),
+            ),
+          ),
+        ),
       ),
     ),
     TO.chainTaskK((blockLag) =>
       KeyValueStore.storeValue(blockLagCacheKey, blockLag),
     ),
-    TO.chainTaskK(() => Db.sqlTNotify("cache-update", blockLagCacheKey)),
+    TO.chainTaskK(() => {
+      Log.debug("sending block-lag cache update");
+      return Db.sqlTNotify("cache-update", blockLagCacheKey);
+    }),
   );
 
 const intervalIterator = setInterval(Duration.millisFromSeconds(2), Date.now());
