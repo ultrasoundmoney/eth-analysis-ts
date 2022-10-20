72d704ee72f223e5585e1c4187626ca89cc59017
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2022-10-20 17:37:12 -0500

    fix: sync missing block terminate

--- src/scripts/add_missing_block.ts
+++ src/scripts/add_missing_block.ts
@@ -1,5 +1,9 @@
 import * as BlocksSync from "../blocks/sync.js";
 import * as Log from "../log.js";
+import * as Db from "../db.js";
+import * as ExecutionNode from "../execution_node.js";
 
-await BlocksSync.syncBlock(13267853);
+await BlocksSync.syncBlock(15116564);
+await ExecutionNode.closeConnections();
+await Db.closeConnection();
 Log.info("done");
