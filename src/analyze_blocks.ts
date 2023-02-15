e66d3b00e29bdd982c910de0744ccecf1ed09a24
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-02-15 08:31:07 +0700

    feat(analyze_blocks): health check

--- src/analyze_blocks.ts
+++ src/analyze_blocks.ts
@@ -1,4 +1,5 @@
 import * as Blocks from "./blocks/blocks.js";
+import Koa from "koa";
 import * as BlockLag from "./block_lag.js";
 import * as BlocksNewBlock from "./blocks/new_head.js";
 import * as BlocksSync from "./blocks/sync.js";
@@ -6,7 +7,7 @@ import * as BurnRecordsSync from "./burn-records/sync.js";
 import * as Config from "./config.js";
 import { runMigrations, sql } from "./db.js";
 import * as ExecutionNode from "./execution_node.js";
-import { pipe, T, TAlt } from "./fp.js";
+import { pipe, T } from "./fp.js";
 import * as LeaderboardsAll from "./leaderboards_all.js";
 import * as LeaderboardsLimitedTimeframe from "./leaderboards_limited_timeframe.js";
 import * as Log from "./log.js";
@@ -15,6 +16,7 @@ import * as PerformanceMetrics from "./performance_metrics.js";
 import * as EthStaked from "./scarcity/eth_staked.js";
 import * as EthSupply from "./scarcity/eth_supply.js";
 import * as SyncOnStart from "./sync_on_start.js";
+import * as Db from "./db.js";
 
 PerformanceMetrics.setShouldLogBlockFetchRate(true);
 
@@ -35,39 +37,77 @@ const initLeaderboardLimitedTimeframes = async (): Promise<void> => {
   Log.info("done loading leaderboards for limited timeframes");
 };
 
+const startHealthCheckServer = async () => {
+  const port = process.env.PORT || 3001;
+
+  const app = new Koa();
+
+  app.on("error", (err) => {
+    Log.error("unhandled error", err);
+  });
+
+  // Health check middleware
+  app.use(async (ctx, next) => {
+    if (
+      ctx.path === "/healthz" ||
+      ctx.path === "/health" ||
+      ctx.path === "/api/fees/healthz"
+    ) {
+      await Db.checkHealth();
+      await ExecutionNode.checkHealth();
+      ctx.res.writeHead(200);
+      ctx.res.end();
+      return undefined;
+    }
+
+    await next();
+    return undefined;
+  });
+
+  await new Promise((resolve) => {
+    app.listen(port, () => {
+      resolve(undefined);
+    });
+  });
+
+  Log.info(`listening on ${port}`);
+};
+
 try {
   Config.ensureCriticalBlockAnalysisConfig();
   await runMigrations();
 
+  await startHealthCheckServer();
+
   const lastStoredBlockOnStart = await Blocks.getLastStoredBlock()();
   const chainHeadOnStart = await ExecutionNode.getLatestBlockNumber();
   Log.debug(`fast-sync blocks up to ${chainHeadOnStart}`);
+
   ExecutionNode.subscribeNewHeads(BlocksNewBlock.onNewBlock);
   Log.debug("listening and queuing new chain heads for analysis");
+
   await BlocksSync.syncBlocks(chainHeadOnStart);
   Log.info("fast-sync blocks done");
 
-  await TAlt.seqTSeq(
-    pipe(
-      BurnRecordsSync.sync(),
-      Performance.measureTaskPerf("sync burn records"),
-    ),
-    () => EthStaked.init(),
-    () => EthSupply.init(),
-    pipe(
-      () => initLeaderboardLimitedTimeframes(),
-      Performance.measureTaskPerf("init leaderboard limited timeframes"),
-    ),
-    pipe(
-      syncLeaderboardAll(),
-      Performance.measureTaskPerf("init leaderboard all"),
-    ),
-    pipe(
-      SyncOnStart.sync(lastStoredBlockOnStart.number + 1, chainHeadOnStart),
-      Performance.measureTaskPerf("sync-next on start"),
-    ),
-    pipe(BlockLag.init, Performance.measureTaskPerf("init block lag")),
-  )();
+  await Performance.measurePromisePerf(
+    "sync burn records",
+    BurnRecordsSync.sync()(),
+  );
+  await EthStaked.init();
+  await EthSupply.init();
+  await Performance.measurePromisePerf(
+    "init leaderboard limited timeframes",
+    initLeaderboardLimitedTimeframes(),
+  );
+  await Performance.measurePromisePerf(
+    "init leaderboard all",
+    syncLeaderboardAll()(),
+  );
+  await Performance.measurePromisePerf(
+    "sync-next on start",
+    SyncOnStart.sync(lastStoredBlockOnStart.number + 1, chainHeadOnStart)(),
+  );
+  await Performance.measurePromisePerf("init block lag", BlockLag.init());
 
   BlocksNewBlock.headsQueue.start();
   Log.info("started analyzing new blocks from queue");
