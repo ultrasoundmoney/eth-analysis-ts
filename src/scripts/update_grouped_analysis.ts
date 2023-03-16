2ddddfba0a9197e82bce34b1a26b41bba51f84fc
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-03-16 11:14:17 +0100

    refactor: formatting

--- src/scripts/update_grouped_analysis.ts
+++ src/scripts/update_grouped_analysis.ts
@@ -15,35 +15,35 @@ Log.info(`Updating grouped analysis for block: ${lastStoredBlock.number}`);
 const oBlock = await Blocks.getBlockByHash(lastStoredBlock.hash)();
 
 if (O.isSome(oBlock)) {
-    const block = oBlock.value;
-    const transactionReceipts = await Transactions.getTxrsWithRetry(block);
-    Log.info("Finished getting transaction receipts");
+  const block = oBlock.value;
+  const transactionReceipts = await Transactions.getTxrsWithRetry(block);
+  Log.info("Finished getting transaction receipts");
 
-    const ethPrice = await pipe(
-        EthPrices.getEthPrice(block.timestamp, Duration.millisFromMinutes(5)),
-        TEAlt.getOrThrow,
-    )();
+  const ethPrice = await pipe(
+    EthPrices.getEthPrice(block.timestamp, Duration.millisFromMinutes(5)),
+    TEAlt.getOrThrow,
+  )();
 
-    const feeSegments = sumFeeSegments(
-        block,
-        Transactions.segmentTransactions(transactionReceipts),
-        ethPrice.ethusd,
-    );
+  const feeSegments = sumFeeSegments(
+    block,
+    Transactions.segmentTransactions(transactionReceipts),
+    ethPrice.ethusd,
+  );
 
-    const tips = BaseFees.calcBlockTips(block, transactionReceipts);
+  const tips = BaseFees.calcBlockTips(block, transactionReceipts);
 
-    const blockDb = Blocks.blockDbFromAnalysis(
-        block,
-        feeSegments,
-        tips,
-        ethPrice.ethusd,
-    );
-    await TAlt.seqTSeq(
-        pipe(
-            GroupedAnalysis1.updateAnalysis(blockDb),
-            Performance.measureTaskPerf("update grouped analysis 1"),
-        ),
-    )();
-    Log.info("Finished updating grouped analysis 1");
-    await closeConnection();
+  const blockDb = Blocks.blockDbFromAnalysis(
+    block,
+    feeSegments,
+    tips,
+    ethPrice.ethusd,
+  );
+  await TAlt.seqTSeq(
+    pipe(
+      GroupedAnalysis1.updateAnalysis(blockDb),
+      Performance.measureTaskPerf("update grouped analysis 1"),
+    ),
+  )();
+  Log.info("Finished updating grouped analysis 1");
+  await closeConnection();
 }
