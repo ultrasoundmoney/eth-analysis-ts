0fcb30496388f5b3569747d5c623bcaf452419a0
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-03-16 11:14:10 +0100

    refactor: clean up imports

--- src/scripts/update_grouped_analysis.ts
+++ src/scripts/update_grouped_analysis.ts
@@ -1,21 +1,12 @@
-import PQueue from "p-queue";
-import { performance } from "perf_hooks";
 import * as BaseFees from "../base_fees.js";
 import { sumFeeSegments } from "../base_fees.js";
-import * as BurnRecordsNewHead from "../burn-records/new_head.js";
-import * as Contracts from "../contracts/contracts.js";
-import * as ContractBaseFees from "../contract_base_fees.js";
-import { closeConnection, sqlTNotify } from "../db.js";
-import * as DeflationaryStreaks from "../deflationary_streaks.js";
+import { closeConnection } from "../db.js";
 import * as Duration from "../duration.js";
 import * as EthPrices from "../eth-prices/index.js";
 import { O, pipe, TAlt, TEAlt } from "../fp.js";
 import * as GroupedAnalysis1 from "../grouped_analysis_1.js";
-import * as LeaderboardsAll from "../leaderboards_all.js";
-import * as LeaderboardsLimitedTimeframe from "../leaderboards_limited_timeframe.js";
 import * as Log from "../log.js";
 import * as Performance from "../performance.js";
-import * as ScarcityCache from "../scarcity/cache.js";
 import * as Transactions from "../transactions.js";
 import * as Blocks from "../blocks/blocks.js";
 
