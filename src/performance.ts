9dd9e7ae584519715044de223998200a1fd5cd34
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2023-03-17 13:18:33 +0100

    refactor(perf): remove unused fns

--- src/performance.ts
+++ src/performance.ts
@@ -17,42 +17,6 @@ export const logPerf = (
   Log.log(level, `${name} took ${took}s`);
 };
 
-export const logPerfT = (name: string, t0: number) => () => () => {
-  logPerf(name, t0);
-};
-
-export function withPerfLogAsync<A, B>(
-  msg: string,
-  fn: (a: A) => Promise<B>,
-): (a: A) => Promise<B>;
-export function withPerfLogAsync<A, B, C>(
-  msg: string,
-  fn: (a: A, b: B) => Promise<C>,
-): (a: A, b: B) => Promise<C> {
-  const t0 = performance.now();
-
-  return async (a: A, b: B) => {
-    const result = await fn(a, b);
-    logPerf(msg, t0);
-    return result;
-  };
-}
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const withPerfLogT =
-  <A extends any[], B>(msg: string, fn: (...args: A) => T.Task<B>) =>
-  (...args: A) =>
-    pipe(
-      T.Do,
-      T.bind("t0", () => T.of(performance.now())),
-      T.bind("result", () => fn(...args)),
-      T.chainFirstIOK(({ t0 }) => () => {
-        logPerf(msg, t0);
-      }),
-      T.map(({ result }) => result),
-    );
-/* eslint-enable @typescript-eslint/no-explicit-any */
-
 export const measureTaskPerf =
   (msg: string) =>
   <A>(task: T.Task<A>) =>
