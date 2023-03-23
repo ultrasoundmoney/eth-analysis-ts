a0ea58785921609c37e592d2f3ab5068d74c4811
ckoopmann <c.k.e.koopmann@gmail.com>
2023-03-23 10:40:04 +0800

    Add script to analyze fixed number of blocks

--- src/blocks/sync.ts
+++ src/blocks/sync.ts
@@ -7,7 +7,7 @@ import * as Transactions from "../transactions.js";
 import * as Blocks from "./blocks.js";
 import { rollbackToIncluding } from "./new_head.js";
 
-export const syncBlock = async (blockNumber: number): Promise<void> => {
+export const syncBlock = async (blockNumber: number, skipParentCheck: boolean = false): Promise<void> => {
   Log.info(`syncing block: ${blockNumber}`);
   const block = await pipe(
     Blocks.getBlockSafe(blockNumber),
@@ -17,7 +17,7 @@ export const syncBlock = async (blockNumber: number): Promise<void> => {
   const isParentKnown = await Blocks.getBlockHashIsKnown(block.parentHash);
 
   // A rollback happened, before or during syncing. Roll back to the last known parent, sync up to the current block, and continue.
-  if (!isParentKnown && !(blockNumber === Blocks.londonHardForkBlockNumber)) {
+  if (!skipParentCheck && !isParentKnown && !(blockNumber === Blocks.londonHardForkBlockNumber)) {
     Log.warn(
       "sync block parent is not in our DB, rolling back one block and trying again",
     );
@@ -37,7 +37,7 @@ export const syncBlock = async (blockNumber: number): Promise<void> => {
     TEAlt.getOrThrow,
   )();
 
-  await Blocks.storeBlock(block, txrs, ethPrice.ethusd);
+  await Blocks.storeBlock(block, txrs, ethPrice.ethusd, skipParentCheck);
 };
 
 const rollbackToLastValidBlock: T.Task<void> = async () => {
@@ -99,6 +99,55 @@ export const syncBlocks = (upToIncluding: number): T.Task<void> =>
 
               for (const blockNumber of blocksToSync) {
                 await syncBlock(blockNumber);
+                blocksDone = blocksDone + 1;
+                logEta();
+              }
+            }),
+          ),
+    ),
+  );
+
+export const syncBlocksStartingFrom = (upToIncluding: number, startBlock: number): T.Task<void> =>
+  pipe(
+    T.Do,
+    // If a rollback happened while we were offline, rollback to the last valid block.
+    T.apS("_rollbackToLastValid", rollbackToLastValidBlock),
+    T.bind("syncedBlockHeight", () => Blocks.getSyncedBlockHeight),
+    T.chain(({ syncedBlockHeight }) =>
+      syncedBlockHeight === upToIncluding
+        ? Log.debugT("blocks table already in-sync with chain")
+        : // Happens sometimes when multiple instances of analyze-blocks are running.
+        syncedBlockHeight > upToIncluding
+        ? T.fromIO(() => {
+            throw new Error(
+              "failed to sync, blocks table is further ahead than requested sync point",
+            );
+          })
+        : pipe(
+            T.Do,
+            T.apS(
+              "blocksToSync",
+              T.of(NEA.range(Math.max(startBlock, syncedBlockHeight) + 1, upToIncluding)),
+            ),
+            T.chain(({ blocksToSync }) => async () => {
+              Log.debug(
+                `blocks table sync ${
+                  blocksToSync.length
+                } blocks, start: ${NEA.head(blocksToSync)}, end: ${NEA.last(
+                  blocksToSync,
+                )}`,
+              );
+
+              const eta = makeEta({ max: blocksToSync.length });
+              let blocksDone = 0;
+
+              const logEta = _.throttle(() => {
+                eta.report(blocksDone);
+                Log.debug(`sync missing blocks, eta: ${eta.estimate()}s`);
+              }, 8000);
+
+              for (const blockNumber of blocksToSync) {
+                await syncBlock(blockNumber, true);
                 blocksDone = blocksDone + 1;
                 logEta();
               }
